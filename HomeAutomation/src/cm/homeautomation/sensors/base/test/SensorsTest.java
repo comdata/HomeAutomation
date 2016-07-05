cbd831425b0f3fd6800f5986d9d6ca7e30d714b6
Christoph Mertins <c.mertins@gmail.com>
2016-07-06 00:08:40 +0200

    additional tests

--- HomeAutomation/src/cm/homeautomation/sensors/base/test/SensorsTest.java
+++ HomeAutomation/src/cm/homeautomation/sensors/base/test/SensorsTest.java
@@ -2,15 +2,27 @@ package cm.homeautomation.sensors.base.test;
 
 import static org.junit.Assert.*;
 
+import java.util.List;
+
 import org.junit.Test;
 
 import cm.homeautomation.sensors.base.Sensors;
+import cm.homeautomation.sensors.base.TechnicalSensor;
 
 public class SensorsTest {
 
 	@Test
 	public void testGetInstance() {
 		assertNotNull(Sensors.getInstance());
+	}
+	
+	@Test
+	public void testGetSensors() {
+		Sensors instance = Sensors.getInstance();
+
+		List<TechnicalSensor> sensors = instance.getSensors();
+		assertNotNull(sensors);
+		assertTrue(sensors.isEmpty());
 	}
 
 }
