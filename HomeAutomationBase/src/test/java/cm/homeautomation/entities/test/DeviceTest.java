bd0d4e639163df23cefa4e74efb675eeea10864b
cmertins <c.mertins@gmail.com>
2016-07-06 10:00:33 +0200

    device test

---
+++ HomeAutomationBase/src/test/java/cm/homeautomation/entities/test/DeviceTest.java
@@ -0,0 +1,38 @@
+package cm.homeautomation.entities.test;
+
+import javax.persistence.EntityManager;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import cm.homeautomation.db.EntityManagerService;
+import cm.homeautomation.entities.Device;
+import cm.homeautomation.entities.Room;
+
+public class DeviceTest {
+
+	private EntityManager em;
+	private Room room;
+
+	@Before
+	public void setup() {
+		em = EntityManagerService.getNewManager();
+		
+		room=new Room();
+		room.setRoomName("Test Room");
+
+		em.persist(room);
+	}
+	
+	@Test
+	public void testCreateDevice() throws Exception {
+		
+		Device device = new Device();
+		
+		device.setMac("00:00:00:00:00");
+		device.setName("Test device");
+		device.setRoom(room);
+		
+		em.persist(device );
+	}
+}
