82159a83982e2a9180e09983e3534cf186c4a351
cmertins <c.mertins@gmail.com>
2016-07-07 21:12:22 +0200

    rename member

--- HomeAutomation/src/cm/homeautomation/eventbus/EventBusEndpointConfigurator.java
+++ HomeAutomation/src/cm/homeautomation/eventbus/EventBusEndpointConfigurator.java
@@ -3,10 +3,11 @@ package cm.homeautomation.eventbus;
 import javax.websocket.server.ServerEndpointConfig.Configurator;
 
 public class EventBusEndpointConfigurator extends Configurator {
-	private static EventBusEndpoint actorEndpoint=new EventBusEndpoint();
+	private static EventBusEndpoint eventBusEndpoint=new EventBusEndpoint();
 
+	@SuppressWarnings("unchecked")
 	@Override
     public <T> T getEndpointInstance(Class<T> endpointClass) throws InstantiationException {
-        return (T)actorEndpoint;
+        return (T)eventBusEndpoint;
     }
 }
