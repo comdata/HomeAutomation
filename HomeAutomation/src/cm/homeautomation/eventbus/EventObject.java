ce9ace6e4541290967ef827b8875a1886ae6165e
cmertins <c.mertins@gmail.com>
2016-07-08 10:40:31 +0200

    add class name to object

--- HomeAutomation/src/cm/homeautomation/eventbus/EventObject.java
+++ HomeAutomation/src/cm/homeautomation/eventbus/EventObject.java
@@ -6,16 +6,30 @@ import javax.xml.bind.annotation.XmlRootElement;
 public class EventObject {
 
 	private Object data;
+	private String clazz;
 	
 	public EventObject(Object data) {
-		this.data = data;
+		setData(data);
 	}
 
 	public Object getData() {
+			
 		return data;
 	}
 
 	public void setData(Object data) {
+		if (data!=null) {
+			setClazz(data.getClass().getSimpleName());
+		}
+		
 		this.data = data;
+	}
+
+	public String getClazz() {
+		return clazz;
+	}
+
+	public void setClazz(String clazz) {
+		this.clazz = clazz;
 	}
 }
