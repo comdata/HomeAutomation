cc4b92c6532041f6d3f27396f5dfacf4b90c8eca
cmertins <c.mertins@gmail.com>
2016-07-18 21:13:42 +0200

    fix test

--- HomeAutomationBase/src/test/java/cm/homeautomation/entities/test/SensorTest.java
+++ HomeAutomationBase/src/test/java/cm/homeautomation/entities/test/SensorTest.java
@@ -14,40 +14,43 @@ import cm.homeautomation.entities.Sensor;
 public class SensorTest {
 	private EntityManager em;
 	private Room room;
-	
 
 	@Before
 	public void setup() {
 		em = EntityManagerService.getNewManager();
 		em.getTransaction().begin();
-		
+
 		room = new Room();
 		room.setRoomName("Test Sensor Room");
 
 		em.persist(room);
 		em.getTransaction().commit();
 	}
-	
+
 	@Test
 	public void testCreateSensor() throws Exception {
 		em.getTransaction().begin();
 		Sensor sensor = new Sensor();
 		sensor.setRoom(room);
 		sensor.setSensorName("Testsensor");
-		
+
 		em.persist(sensor);
 		em.getTransaction().commit();
 
 		assertTrue("Id: " + sensor.getId(), sensor.getId() != null);
 	}
 
-	@Test(expected=org.eclipse.persistence.exceptions.DatabaseException.class)
-	public void testCreateEmptySensor() throws Exception {
-		em.getTransaction().begin();
-		Sensor sensor = new Sensor();
-		sensor.setRoom(room);
-		em.persist(sensor);
-		em.getTransaction().commit();
-		assertTrue("Id: "+sensor.getId(), sensor.getId()==null);
+	@Test
+	public void testCreateEmptySensor() {
+		try {
+			em.getTransaction().begin();
+			Sensor sensor = new Sensor();
+			sensor.setRoom(room);
+			em.persist(sensor);
+			em.getTransaction().commit();
+		} catch (Exception e) {
+			assertTrue(e instanceof javax.persistence.RollbackException);
+		}
+
 	}
 }
