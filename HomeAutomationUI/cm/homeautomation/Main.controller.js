71610e408aa5eacca1013bbae14b9598d53a99bb
Christoph Mertins <c.mertins@gmail.com>
2016-07-26 20:34:55 +0200

    indent

--- HomeAutomationUI/cm/homeautomation/Main.controller.js
+++ HomeAutomationUI/cm/homeautomation/Main.controller.js
@@ -156,8 +156,8 @@ sap.ui.define([
         
         handleTransmissionStatus: function (data) {
         	if (this.transmissionTile!=null) {
-        	this.transmissionTile.number=data.numberOfDoneTorrents+" / "+data.numberOfTorrents;
-        	this.transmissionTile.info=Math.round(data.downloadSpeed/1024*100)/100+" / "+Math.round(data.uploadSpeed/1024*100)/100 + " kb";
+        		this.transmissionTile.number=data.numberOfDoneTorrents+" / "+data.numberOfTorrents;
+        		this.transmissionTile.info=Math.round(data.downloadSpeed/1024*100)/100+" / "+Math.round(data.uploadSpeed/1024*100)/100 + " kb";
         	}
         },
 
