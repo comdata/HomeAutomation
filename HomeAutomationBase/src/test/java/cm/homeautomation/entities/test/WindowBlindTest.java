2c6fa62e5a7cd6d87895345b32881a69b90bceb8
Christoph Mertins <c.mertins@gmail.com>
2016-08-03 19:18:14 +0200

    add additional windowblind tests

--- HomeAutomationBase/src/test/java/cm/homeautomation/entities/test/WindowBlindTest.java
+++ HomeAutomationBase/src/test/java/cm/homeautomation/entities/test/WindowBlindTest.java
@@ -1,28 +1,37 @@
 package cm.homeautomation.entities.test;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 import javax.persistence.EntityManager;
+import javax.persistence.PersistenceException;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 import cm.homeautomation.db.EntityManagerService;
+import cm.homeautomation.entities.Room;
 import cm.homeautomation.entities.WindowBlind;
 
 public class WindowBlindTest {
 	private EntityManager em;
 	private String calibrationUrl="http://www.google.de";
 	private WindowBlind windowBlind;
+	private Room room;
 	
 	@Before
 	public void setup() {
-		
+		em = EntityManagerService.getNewManager();
+		em.getTransaction().begin();
+		room = new Room();
+		room.setRoomName("Test Sensor Room");
+
+		em.persist(room);
 	}
 	
 	@After
 	public void tearDown() {
+		em.getTransaction().commit();
 		em = EntityManagerService.getNewManager();
 		em.getTransaction().begin();
 		windowBlind=em.merge(windowBlind);
@@ -31,13 +40,32 @@ public class WindowBlindTest {
 	}
 	
 	@Test
+	public void testCreateBlind() throws Exception {
+		windowBlind = new WindowBlind();
+		
+		windowBlind.setName("Testblind");
+		windowBlind.setRoom(room);
+		em.persist(windowBlind);
+		em.getTransaction().commit();
+	}
+	
+	@Test(expected=PersistenceException.class)
+	public void testWindowBlindFailsWithoutRoom() throws Exception {
+		windowBlind = new WindowBlind();
+		
+		windowBlind.setName("Testblind");
+		em.persist(windowBlind);
+		em.getTransaction().commit();		
+	}
+	
+	@Test
 	public void testCalibrationURL() throws Exception {
 		
-		em = EntityManagerService.getNewManager();
-		em.getTransaction().begin();
+		
 		windowBlind = new WindowBlind();
 		
 		windowBlind.setCalibrationUrl(calibrationUrl);
+		windowBlind.setRoom(room);
 		em.persist(windowBlind);
 		em.getTransaction().commit();
 		
