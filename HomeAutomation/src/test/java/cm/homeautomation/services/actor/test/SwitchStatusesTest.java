6842d8f57f41a9a911c8c00232a36c364b60a9d1
c.mertins <c.mertins@gmail.com>
2016-08-18 23:12:08 +0200

    add switch tests

---
+++ HomeAutomation/src/test/java/cm/homeautomation/services/actor/test/SwitchStatusesTest.java
@@ -0,0 +1,38 @@
+package cm.homeautomation.services.actor.test;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+import cm.homeautomation.entities.Switch;
+import cm.homeautomation.services.actor.SwitchStatuses;
+
+public class SwitchStatusesTest {
+
+	@Test
+	public void testCreate() throws Exception {
+		SwitchStatuses switchStatuses = new SwitchStatuses();
+		
+		assertNotNull(switchStatuses);
+		assertNotNull(switchStatuses.getSwitchStatuses());
+		assertTrue(switchStatuses.getSwitchStatuses().isEmpty());
+	}
+	
+	@Test
+	public void testCreateWithEntries() throws Exception {
+		SwitchStatuses switchStatuses = new SwitchStatuses();
+		
+		List<Switch> switchStatusList=new ArrayList<Switch>();
+		Switch aSwitch=new Switch();
+		aSwitch.setName("Test Switch");
+		switchStatusList.add(aSwitch);
+		switchStatuses.setSwitchStatuses(switchStatusList);
+		assertNotNull(switchStatuses);
+		assertNotNull(switchStatuses.getSwitchStatuses());
+		assertTrue(switchStatuses.getSwitchStatuses().size()==1);
+		assertEquals(aSwitch, switchStatuses.getSwitchStatuses().get(0));
+	}
+}
