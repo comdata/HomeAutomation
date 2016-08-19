967c04c7af090b9475b837adc94ef61679b926e3
Christoph Mertins <c.mertins@gmail.com>
2016-08-19 10:44:10 +0200

    initial test for sensor service

---
+++ HomeAutomation/src/test/java/cm/homeautomation/services/sensors/test/SensorsTest.java
@@ -0,0 +1,53 @@
+package cm.homeautomation.services.sensors.test;
+
+import static org.junit.Assert.*;
+
+import javax.persistence.EntityManager;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import cm.homeautomation.db.EntityManagerService;
+import cm.homeautomation.entities.Room;
+import cm.homeautomation.entities.Switch;
+import cm.homeautomation.sensors.RFEvent;
+import cm.homeautomation.services.sensors.Sensors;
+
+
+public class SensorsTest {
+	
+	private Sensors sensors;
+	private EntityManager em;
+	private Room room;
+
+	@Before
+	public void setup() {
+		sensors = new Sensors();
+		em = EntityManagerService.getNewManager();
+		
+		em.getTransaction().begin();
+		room = new Room();
+		room.setRoomName("rf switch room");
+		
+		em.persist(room);
+		
+		em.getTransaction().commit();
+	}
+	
+	@Test
+	public void testRFEvent() throws Exception {
+		int onCode = 1111;
+		Switch rfSwitch = new Switch();
+		rfSwitch.setOnCode(""+onCode);
+		rfSwitch.setRoom(room);
+		rfSwitch.setName("Test switch");
+		
+		em.getTransaction().begin();
+		em.persist(rfSwitch);
+		em.getTransaction().commit();
+		
+		RFEvent rfEvent=new RFEvent();
+		rfEvent.setCode(onCode);
+		sensors.registerRFEvent(rfEvent);
+	}
+}
