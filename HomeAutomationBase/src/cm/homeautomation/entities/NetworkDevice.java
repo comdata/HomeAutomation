d88119e3b8c93c75dc9600212401cd657a7311e2
Christoph Mertins <c.mertins@gmail.com>
2016-08-28 20:05:58 +0200

    add network devices as entities

---
+++ HomeAutomationBase/src/cm/homeautomation/entities/NetworkDevice.java
@@ -0,0 +1,35 @@
+package cm.homeautomation.entities;
+
+import javax.persistence.Entity;
+
+@Entity
+public class NetworkDevice {
+
+	private String mac;
+	private String ip;
+	private String hostname;
+
+	public String getMac() {
+		return mac;
+	}
+
+	public void setMac(String mac) {
+		this.mac = mac;
+	}
+
+	public String getIp() {
+		return ip;
+	}
+
+	public void setIp(String ip) {
+		this.ip = ip;
+	}
+
+	public String getHostname() {
+		return hostname;
+	}
+
+	public void setHostname(String hostname) {
+		this.hostname = hostname;
+	}
+}
