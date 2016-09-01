d43f96ccc3344f9217fd7e6f24e1394172c419cd
Christoph Mertins <c.mertins@gmail.com>
2016-09-01 11:58:04 +0200

    add test for tv binding

---
+++ HomeAutomationBase/src/test/java/com/homeautomation/tv/panasonic/test/PanasonicTVBindingTest.java
@@ -0,0 +1,101 @@
+package com.homeautomation.tv.panasonic.test;
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.homeautomation.tv.panasonic.PanasonicTVBinding;
+
+public class PanasonicTVBindingTest {
+
+	private final int tvPort = 55000;
+	private final String tvIp = "127.0.0.1";
+	private TVServer tvServer;
+
+	@Before
+	public void setup() {
+		tvServer = new TVServer();
+	}
+	
+	@After
+	public void tearDown() {
+		tvServer.setRun(false);
+	}
+	
+	@Test
+	public void testCheckAlive() throws Exception {
+		tvServer.setStatusCode("200 OK");
+		PanasonicTVBinding panasonicTVBinding = new PanasonicTVBinding();
+		boolean checkAlive = panasonicTVBinding.checkAlive(tvIp);
+		
+		assertTrue(checkAlive);
+	}
+	
+	@Test
+	public void testCheckAliveFalse() throws Exception {
+		tvServer.setStatusCode("404 Not found");
+		PanasonicTVBinding panasonicTVBinding = new PanasonicTVBinding();
+		boolean checkAlive = panasonicTVBinding.checkAlive(tvIp);
+		
+		assertTrue(checkAlive);
+	}
+
+	class TVServer extends Thread {
+
+		private boolean run = true;
+		private String statusCode = "";
+
+		public void run() {
+			while (run) {
+
+				try {
+					String clientSentence;
+					ServerSocket welcomeSocket = new ServerSocket(tvPort);
+					Socket connectionSocket = welcomeSocket.accept();
+					BufferedReader inFromClient = new BufferedReader(
+							new InputStreamReader(connectionSocket.getInputStream()));
+					DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+
+					boolean clientData = true;
+					while (clientData) {
+						clientSentence = inFromClient.readLine();
+						if (clientSentence.equals("")) {
+							clientData = false;
+						}
+					}
+
+					outToClient.writeBytes(statusCode);
+
+					welcomeSocket.close();
+
+				} catch (IOException e) {
+				}
+			}
+		}
+
+		public boolean isRun() {
+			return run;
+		}
+
+		public void setRun(boolean run) {
+			this.run = run;
+		}
+
+		public String getStatusCode() {
+			return statusCode;
+		}
+
+		public void setStatusCode(String statusCode) {
+			this.statusCode = statusCode;
+		}
+	}
+}
