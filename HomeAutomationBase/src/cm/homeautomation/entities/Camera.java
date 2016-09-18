056c7d9997b7c83acaf165aafbf029fc803513ed
c.mertins <c.mertins@gmail.com>
2016-09-18 18:09:10 +0200

    add camera entities

---
+++ HomeAutomationBase/src/cm/homeautomation/entities/Camera.java
@@ -0,0 +1,63 @@
+package cm.homeautomation.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+@Entity
+public class Camera {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@JsonBackReference("room")
+	@ManyToOne
+	@JoinColumn(name = "ROOM_ID", nullable = false)
+	private Room room;
+
+	@Column(nullable=false, unique=true)
+	private String cameraName;
+	
+	private String icon;
+	private String stream;
+
+	public String getCameraName() {
+		return cameraName;
+	}
+
+	public void setCameraName(String cameraName) {
+		this.cameraName = cameraName;
+	}
+
+	public Room getRoom() {
+		return room;
+	}
+
+	public void setRoom(Room room) {
+		this.room = room;
+	}
+
+	public String getStream() {
+		return stream;
+	}
+
+	public void setStream(String stream) {
+		this.stream = stream;
+	}
+
+	public String getIcon() {
+		return icon;
+	}
+
+	public void setIcon(String icon) {
+		this.icon = icon;
+	}
+
+}
