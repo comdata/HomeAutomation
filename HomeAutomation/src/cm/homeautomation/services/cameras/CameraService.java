9a9ef1b86e1d24381c6947fff69115e81f273575
Christoph Mertins <c.mertins@gmail.com>
2016-09-19 15:59:53 +0200

    change response

--- HomeAutomation/src/cm/homeautomation/services/cameras/CameraService.java
+++ HomeAutomation/src/cm/homeautomation/services/cameras/CameraService.java
@@ -5,6 +5,7 @@ import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.URL;
 import java.util.List;
 
@@ -13,6 +14,9 @@ import javax.persistence.EntityManager;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.StreamingOutput;
 
 import cm.homeautomation.db.EntityManagerService;
 import cm.homeautomation.entities.Camera;
@@ -39,16 +43,31 @@ public class CameraService extends BaseService {
 	}
 	
 	@Path("getSnapshot/{id}")
-	@Produces("image/jpg")
-	public byte[] getSnapshot(@PathParam("id") Long id) {
+	@Produces("image/jpeg")
+	public Response getSnapshot(@PathParam("id") Long id) {
 		EntityManager em = EntityManagerService.getManager();
 		List<Camera> resultList = (List<Camera>)em.createQuery("select c from Camera c where c.id=:id")
 				.setParameter("id", id).getResultList();
 		
 		for (Camera camera : resultList) {
-			return camera.getImageSnapshot();
+			final byte[] imageData = camera.getImageSnapshot();
+			StreamingOutput stream = new StreamingOutput() {
+
+				@Override
+				public void write(OutputStream output) throws IOException,
+						WebApplicationException {
+					try {
+						output.write(imageData);
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+
+				}
+			};
+
+			return Response.ok(stream).build();
 		}
-		return null;
+		return Response.serverError().build();
 	}
 
 	public static void prepareCameraImage(String[] args) {
