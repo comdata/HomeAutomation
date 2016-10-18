3f626bf3fb152cd50bd2946c4f1d117a81c9f695
Christoph Mertins <c.mertins@gmail.com>
2016-10-18 22:36:30 +0200

    LED Service and color picker

---
+++ HomeAutomation/src/cm/homeautomation/services/led/LEDService.java
@@ -0,0 +1,43 @@
+package cm.homeautomation.services.led;
+
+import java.io.IOException;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import cm.homeautomation.services.base.BaseService;
+
+@Path("led")
+public class LEDService extends BaseService {
+
+	@GET
+	public void setLed(int r, int g, int b) {
+
+		try {
+			String url = "http://192.168.1.38:80/?mode=SET&red=" + r + "&green=" + g + "&blue=" + b;
+			HttpGet getMethod = new HttpGet(url);
+			HttpClientBuilder clientBuilder = HttpClientBuilder.create();
+
+			// String[] userPassword = url.split("@")[0].replace("http://",
+			// "").split(":");
+			/*
+			 * CredentialsProvider credsProvider = new
+			 * BasicCredentialsProvider(); credsProvider.setCredentials(new
+			 * AuthScope(getMethod.getURI().getHost(),
+			 * getMethod.getURI().getPort()), new
+			 * UsernamePasswordCredentials(userPassword[0], userPassword[1]));
+			 * clientBuilder.setDefaultCredentialsProvider(credsProvider);
+			 */
+			HttpClient httpClient = clientBuilder.build();
+
+			httpClient.execute(getMethod);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
