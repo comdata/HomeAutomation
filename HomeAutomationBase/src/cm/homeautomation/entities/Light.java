b3a838aed2ab270b64801c054e7742dcb3acacea
Christoph Mertins <c.mertins@gmail.com>
2016-11-10 20:10:27 +0100

    add getter and setter

--- HomeAutomationBase/src/cm/homeautomation/entities/Light.java
+++ HomeAutomationBase/src/cm/homeautomation/entities/Light.java
@@ -65,5 +65,13 @@ public class Light {
 	public void setName(String name) {
 		this.name = name;
 	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
 	
 }
