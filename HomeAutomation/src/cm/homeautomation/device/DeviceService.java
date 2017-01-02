c7c1f56c2e71481e5b7aff1d03e4bfe67fa80929
Christoph Mertins <c.mertins@gmail.com>
2017-01-02 23:06:21 +0100

    change finding of room

--- HomeAutomation/src/cm/homeautomation/device/DeviceService.java
+++ HomeAutomation/src/cm/homeautomation/device/DeviceService.java
@@ -1,21 +1,26 @@
 package cm.homeautomation.device;
 
-import java.util.List;
-
 import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
 
 import cm.homeautomation.db.EntityManagerService;
 import cm.homeautomation.entities.Device;
 import cm.homeautomation.entities.Room;
-import cm.homeautomation.entities.Sensor;
 
 public class DeviceService {
 
 	public static Long getRoomIdForMac(String mac) {
 		Long roomId = null;
 
-		if (roomId == null && mac != null) {
+		Room room = getRoomForMac(mac);
+		if (room!=null) {
+			roomId=room.getId();
+		}
+		return roomId;
+	}
+
+	public static Room getRoomForMac(String mac) {
+
 			EntityManager em = EntityManagerService.getNewManager();
 
 			try {
@@ -28,30 +33,12 @@ public class DeviceService {
 					return null;
 				}
 
-				roomId = device.getRoom().getId();
+				return device.getRoom();
+				
 
 			} catch (NoResultException e) {
 				return null;
 			}
-
-		}
-		return roomId;
-	}
-
-	public static Room getRoomForMac(String mac) {
-		Long roomId = getRoomIdForMac(mac);
-
-		if (roomId != null) {
-
-			EntityManager em = EntityManagerService.getNewManager();
-			List<Room> roomList = em.createQuery("select r from Room r where r.id=:roomId")
-					.setParameter("roomId", roomId).getResultList();
-
-			for (Room room : roomList) {
-				return room;
-			}
-		}
-		return null;
 	}
 
 }
