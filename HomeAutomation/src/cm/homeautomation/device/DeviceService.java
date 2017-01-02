c4f153969576b7135117e0ffd3a2ed03d28e04a4
Christoph Mertins <c.mertins@gmail.com>
2017-01-02 21:56:19 +0100

    handle room not set

--- HomeAutomation/src/cm/homeautomation/device/DeviceService.java
+++ HomeAutomation/src/cm/homeautomation/device/DeviceService.java
@@ -3,6 +3,7 @@ package cm.homeautomation.device;
 import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
 
 import cm.homeautomation.db.EntityManagerService;
 import cm.homeautomation.entities.Device;
@@ -12,33 +13,45 @@ import cm.homeautomation.entities.Sensor;
 public class DeviceService {
 
 	public static Long getRoomIdForMac(String mac) {
-		Long roomId=null;
-		
-		if (roomId==null && mac!=null) {
-			EntityManager em=EntityManagerService.getNewManager();
-			
-			Device device = (Device) em.createQuery("select d from Device d where d.mac=:mac").setParameter("mac", mac).getSingleResult();
-			
-			if (device==null) {
-				
+		Long roomId = null;
+
+		if (roomId == null && mac != null) {
+			EntityManager em = EntityManagerService.getNewManager();
+
+			try {
+
+				Device device = (Device) em.createQuery("select d from Device d where d.mac=:mac")
+						.setParameter("mac", mac).getSingleResult();
+
+				if (device == null) {
+
+					return null;
+				}
+
+				roomId = device.getRoom().getId();
+
+			} catch (NoResultException e) {
 				return null;
 			}
-			
-			roomId=device.getRoom().getId();
+
 		}
 		return roomId;
 	}
-	
+
 	public static Room getRoomForMac(String mac) {
 		Long roomId = getRoomIdForMac(mac);
-		
-		EntityManager em=EntityManagerService.getNewManager();
-		List<Room> roomList = em.createQuery("select r from Room r where r.id=:roomId").setParameter("roomId", roomId).getResultList();
-	
-		for (Room room : roomList) {
-			return room;
+
+		if (roomId != null) {
+
+			EntityManager em = EntityManagerService.getNewManager();
+			List<Room> roomList = em.createQuery("select r from Room r where r.id=:roomId")
+					.setParameter("roomId", roomId).getResultList();
+
+			for (Room room : roomList) {
+				return room;
+			}
 		}
 		return null;
 	}
-	
+
 }
