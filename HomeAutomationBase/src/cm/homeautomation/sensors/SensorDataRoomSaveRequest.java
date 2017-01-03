2d0c59db1de2637ce0a80987587c0befd969248d
Christoph Mertins <c.mertins@gmail.com>
2017-01-03 11:19:05 +0100

    change type info handling

--- HomeAutomationBase/src/cm/homeautomation/sensors/SensorDataRoomSaveRequest.java
+++ HomeAutomationBase/src/cm/homeautomation/sensors/SensorDataRoomSaveRequest.java
@@ -1,5 +1,8 @@
 package cm.homeautomation.sensors;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
+@JsonTypeInfo(use=JsonTypeInfo.Id.CLASS, include=JsonTypeInfo.As.PROPERTY, property="@class")
 public class SensorDataRoomSaveRequest extends JSONSensorDataBase {
 	private Long roomID;
 	private SensorDataRoomPayload data;
