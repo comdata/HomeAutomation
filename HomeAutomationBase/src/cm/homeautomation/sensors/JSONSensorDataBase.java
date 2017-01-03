f5b39e3ca99f9cbb789291385ed2b8a4a52a9aef
Christoph Mertins <c.mertins@gmail.com>
2017-01-03 11:33:18 +0100

    fix type information

--- HomeAutomationBase/src/cm/homeautomation/sensors/JSONSensorDataBase.java
+++ HomeAutomationBase/src/cm/homeautomation/sensors/JSONSensorDataBase.java
@@ -6,7 +6,7 @@ import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonSubTypes.Type;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 
-
+@JsonTypeInfo(use=JsonTypeInfo.Id.CLASS, include=JsonTypeInfo.As.PROPERTY, property="@class")
 @JsonSubTypes({@Type(value=SensorDataRoomSaveRequest.class), @Type(value=SensorDataSaveRequest.class), @Type(value=WindowSensorData.class)})
 @XmlRootElement
 public abstract class JSONSensorDataBase {
