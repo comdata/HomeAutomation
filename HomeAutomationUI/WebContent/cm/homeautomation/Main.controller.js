df7106f8dc9037ccc791d90a8bb30d49b28a149c
Christoph Mertins <c.mertins@gmail.com>
2017-01-05 14:32:38 +0100

    change to bardata

--- HomeAutomationUI/WebContent/cm/homeautomation/Main.controller.js
+++ HomeAutomationUI/WebContent/cm/homeautomation/Main.controller.js
@@ -1188,28 +1188,6 @@ sap.ui.define([
         			};
         	
         	var chartJSData={
-        			  "lineData": {
-        				    "labels": ["January", "February", "March", "April", "May", "June", "July"],
-        				    "datasets": [{
-        				      "label": "My First dataset",
-        				      "fillColor": "rgba(220,220,220,0.2)",
-        				      "strokeColor": "rgba(220,220,220,1)",
-        				      "pointColor": "rgba(220,220,220,1)",
-        				      "pointStrokeColor": "#fff",
-        				      "pointHighlightFill": "#fff",
-        				      "pointHighlightStroke": "rgba(220,220,220,1)",
-        				      "data": [65, 59, 80, 81, 56, 55, 40]
-        				    }, {
-        				      "label": "My Second dataset",
-        				      "fillColor": "rgba(151,187,205,0.2)",
-        				      "strokeColor": "rgba(151,187,205,1)",
-        				      "pointColor": "rgba(151,187,205,1)",
-        				      "pointStrokeColor": "#fff",
-        				      "pointHighlightFill": "#fff",
-        				      "pointHighlightStroke": "rgba(151,187,205,1)",
-        				      "data": [28, 48, 40, 19, 86, 27, 90]
-        				    }]
-        				  },
         				 "barData": {
         				    "labels": ["January", "February", "March", "April", "May", "June", "July"],
         				    "datasets": [{
@@ -1227,71 +1205,7 @@ sap.ui.define([
         				      "highlightStroke": "rgba(151,187,205,1)",
         				      "data": [28, 48, 40, 19, 86, 27, 90]
         				    }]
-        				  },
-        				  "radarData": {
-        				    "labels": ["Eating", "Drinking", "Sleeping", "Designing", "Coding", "Cycling", "Running"],
-        				    "datasets": [{
-        				      "label": "My First dataset",
-        				      "fillColor": "rgba(220,220,220,0.2)",
-        				      "strokeColor": "rgba(220,220,220,1)",
-        				      "pointColor": "rgba(220,220,220,1)",
-        				      "pointStrokeColor": "#fff",
-        				      "pointHighlightFill": "#fff",
-        				      "pointHighlightStroke": "rgba(220,220,220,1)",
-        				      "data": [65, 59, 90, 81, 56, 55, 40]
-        				    }, {
-        				      "label": "My Second dataset",
-        				      "fillColor": "rgba(151,187,205,0.2)",
-        				      "strokeColor": "rgba(151,187,205,1)",
-        				      "pointColor": "rgba(151,187,205,1)",
-        				      "pointStrokeColor": "#fff",
-        				      "pointHighlightFill": "#fff",
-        				      "pointHighlightStroke": "rgba(151,187,205,1)",
-        				      "data": [28, 48, 40, 19, 96, 27, 100]
-        				    }]
-        				  },
-        				  "polarData": [{
-        				    "value": 300,
-        				    "color": "#F7464A",
-        				    "highlight": "#FF5A5E",
-        				    "label": "Red"
-        				  }, {
-        				    "value": 50,
-        				    "color": "#46BFBD",
-        				    "highlight": "#5AD3D1",
-        				    "label": "Green"
-        				  }, {
-        				    "value": 100,
-        				    "color": "#FDB45C",
-        				    "highlight": "#FFC870",
-        				    "label": "Yellow"
-        				  }, {
-        				    "value": 40,
-        				    "color": "#949FB1",
-        				    "highlight": "#A8B3C5",
-        				    "label": "Grey"
-        				  }, {
-        				    "value": 120,
-        				    "color": "#4D5360",
-        				    "highlight": "#616774",
-        				    "label": "Dark Grey"
-        				  }],
-        				  "pieData": [{
-        				    "value": 300,
-        				    "color": "#F7464A",
-        				    "highlight": "#FF5A5E",
-        				    "label": "Red"
-        				  }, {
-        				    "value": 50,
-        				    "color": "#46BFBD",
-        				    "highlight": "#5AD3D1",
-        				    "label": "Green"
-        				  }, {
-        				    "value": 100,
-        				    "color": "#FDB45C",
-        				    "highlight": "#FFC870",
-        				    "label": "Yellow"
-        				  }]
+        				  }
         				};
         	
             var chartJSModel = new JSONModel();
