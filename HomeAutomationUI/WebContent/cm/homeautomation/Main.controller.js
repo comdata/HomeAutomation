df27f8e88aa783dd44089cb65cf8d3e0d0176b96
Christoph Mertins <c.mertins@gmail.com>
2017-01-05 14:18:46 +0100

    change data

--- HomeAutomationUI/WebContent/cm/homeautomation/Main.controller.js
+++ HomeAutomationUI/WebContent/cm/homeautomation/Main.controller.js
@@ -1183,8 +1183,14 @@ sap.ui.define([
         },
         powerMeterLoad: function () {
         	var c3jsData={
-        			  "data1": [30, 200, 100, 400, 150, 250],
-        			  "data2": [50, 20, 10, 40, 15, 25]
+        	        x: 'x',
+//        	        xFormat: '%Y%m%d', // 'xFormat' can be used as custom format of 'x'
+        	        columns: [
+        	            ['x', '2013-01-01', '2013-01-02', '2013-01-03', '2013-01-04', '2013-01-05', '2013-01-06'],
+//        	            ['x', '20130101', '20130102', '20130103', '20130104', '20130105', '20130106'],
+        	            ['data1', 30, 200, 100, 400, 150, 250],
+        	            ['data2', 130, 340, 200, 500, 250, 350]
+        	        ]
         			};
         	
         	var chartJSData={
@@ -1210,7 +1216,7 @@ sap.ui.define([
         				      "data": [28, 48, 40, 19, 86, 27, 90]
         				    }]
         				  },
-        				  "barData": {
+        				 "barData": {
         				    "labels": ["January", "February", "March", "April", "May", "June", "July"],
         				    "datasets": [{
         				      "label": "My First dataset",
