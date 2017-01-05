9bfc53f9ee9c90d1a138c1b6d287bd1db86c3d5a
Christoph Mertins <c.mertins@gmail.com>
2017-01-05 16:21:37 +0100

    add randomizer

--- HomeAutomationUI/WebContent/cm/homeautomation/Main.controller.js
+++ HomeAutomationUI/WebContent/cm/homeautomation/Main.controller.js
@@ -1196,14 +1196,14 @@ sap.ui.define([
         				      "strokeColor": "rgba(220,220,220,0.8)",
         				      "highlightFill": "rgba(220,220,220,0.75)",
         				      "highlightStroke": "rgba(220,220,220,1)",
-        				      "data": [65, 59, 80, 81, 56, 55, 40]
+        				      "data": [Math.randon(1,100), Math.randon(1,100), Math.randon(1,100), Math.randon(1,100), Math.randon(1,100), Math.randon(1,100), Math.randon(1,100)]
         				    }, {
         				      "label": "My Second dataset",
         				      "fillColor": "rgba(151,187,205,0.5)",
         				      "strokeColor": "rgba(151,187,205,0.8)",
         				      "highlightFill": "rgba(151,187,205,0.75)",
         				      "highlightStroke": "rgba(151,187,205,1)",
-        				      "data": [28, 48, 40, 19, 86, 27, 90]
+        				      "data": [Math.randon(1,100), Math.randon(1,100), Math.randon(1,100), Math.randon(1,100), Math.randon(1,100), Math.randon(1,100), Math.randon(1,100)]
         				    }]
         				  }
         				};
