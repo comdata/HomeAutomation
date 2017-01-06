e8a445eb0da0e390ea92381986ae3d8c9a24d270
Christoph Mertins <c.mertins@gmail.com>
2017-01-06 21:10:13 +0100

    change

--- HomeAutomationUI/WebContent/cm/homeautomation/Main.controller.js
+++ HomeAutomationUI/WebContent/cm/homeautomation/Main.controller.js
@@ -1235,7 +1235,7 @@ sap.ui.define([
         				                      "displayFormats": {
         				                          "quarter": "DD.MM.YYYY HH:mm"
         				                      },
-        				                   "unitStepSize": 10
+        				                   "unitStepSize": 30
         				                  }
         				              }]
         				          }
