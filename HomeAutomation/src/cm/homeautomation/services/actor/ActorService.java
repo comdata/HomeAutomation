165b16e7654135823070d19f119878d1621f6f0e
Christoph Mertins <c.mertins@gmail.com>
2017-02-15 07:45:09 +0100

    allow updating of back switch states

--- HomeAutomation/src/cm/homeautomation/services/actor/ActorService.java
+++ HomeAutomation/src/cm/homeautomation/services/actor/ActorService.java
@@ -128,20 +128,10 @@ public class ActorService extends BaseService implements MqttCallback {
 	@Path("press/{switch}/{status}")
 	public SwitchPressResponse pressSwitch(@PathParam("switch") String switchId,
 			@PathParam("status") String targetStatus) {
-		EntityManager em = EntityManagerService.getNewManager();
-
-		Switch singleSwitch = (Switch) em.createQuery("select sw from Switch sw where sw.id=:switchId")
-				.setParameter("switchId", Float.parseFloat(switchId)).getSingleResult();
-
-		String status = "";
-		if ("ON".equals(targetStatus)) {
-			status = singleSwitch.getSwitchOnCode();
-		} else if ("OFF".equals(targetStatus)) {
-			status = singleSwitch.getSwitchOffCode();
-		} else {
-			// TODO fail
-		}
-
+		targetStatus=targetStatus.toUpperCase();
+		
+		Switch singleSwitch = updateBackendSwitchState(switchId, targetStatus);
+		
 		ActorMessage actorMessage = new ActorMessage();
 		actorMessage.setHouseCode(singleSwitch.getHouseCode());
 		actorMessage.setStatus((targetStatus.equals("ON") ? "1" : "0"));
@@ -150,15 +140,37 @@ public class ActorService extends BaseService implements MqttCallback {
 		//sendMulticastUDP(actorMessage);
 		sendMQTTMessage(actorMessage);
 		
+		SwitchPressResponse switchPressResponse = new SwitchPressResponse();
+		switchPressResponse.setSuccess(true);
+		return switchPressResponse;
+	}
+
+	/**
+	 * update backend switch state
+	 * 
+	 * @param switchId
+	 * @param targetStatus
+	 * @return
+	 */
+	@GET
+	@Path("updateBackend/{switch}/{status}")
+	private Switch updateBackendSwitchState(@PathParam("switch") String switchId,
+			@PathParam("status") String targetStatus) {
+		
+		targetStatus=targetStatus.toUpperCase();
+		
+		EntityManager em = EntityManagerService.getNewManager();
+
+		Switch singleSwitch = (Switch) em.createQuery("select sw from Switch sw where sw.id=:switchId")
+				.setParameter("switchId", Float.parseFloat(switchId)).getSingleResult();
+
+	
 		em.getTransaction().begin();
 		singleSwitch.setLatestStatus(targetStatus);
 		singleSwitch.setLatestStatusFrom(new Date());
 		em.merge(singleSwitch);
 		em.getTransaction().commit();
-		
-		SwitchPressResponse switchPressResponse = new SwitchPressResponse();
-		switchPressResponse.setSuccess(true);
-		return switchPressResponse;
+		return singleSwitch;
 	}
 
 	private void sendMQTTMessage(ActorMessage actorMessage) {
