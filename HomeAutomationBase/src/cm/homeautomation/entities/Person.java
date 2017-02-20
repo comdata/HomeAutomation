ec8b1499d1087a5f184dcb49b1a5699dfed9d044
Christoph Mertins <c.mertins@gmail.com>
2017-02-20 21:32:41 +0100

    add id generation

--- HomeAutomationBase/src/cm/homeautomation/entities/Person.java
+++ HomeAutomationBase/src/cm/homeautomation/entities/Person.java
@@ -1,12 +1,15 @@
 package cm.homeautomation.entities;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Person {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 		
 	private String name;
--- HomeAutomationBase/src/cm/homeautomation/entities/PresenceState.java
+++ HomeAutomationBase/src/cm/homeautomation/entities/PresenceState.java
@@ -3,6 +3,8 @@ package cm.homeautomation.entities;
 import java.util.Date;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 
@@ -10,6 +12,7 @@ import javax.persistence.ManyToOne;
 public class PresenceState {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
 	@ManyToOne
