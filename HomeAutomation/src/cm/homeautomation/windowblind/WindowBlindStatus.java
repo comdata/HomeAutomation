53be5213c4422cb7a01898af450789564ad296c8
Christoph Mertins <c.mertins@gmail.com>
2017-02-21 07:30:48 +0100

    make window blind status bot compatible

--- HomeAutomation/src/cm/homeautomation/windowblind/WindowBlindStatus.java
+++ HomeAutomation/src/cm/homeautomation/windowblind/WindowBlindStatus.java
@@ -2,10 +2,11 @@ package cm.homeautomation.windowblind;
 
 import javax.xml.bind.annotation.XmlRootElement;
 
+import cm.homeautomation.entities.HumanMessageGenerationInterface;
 import cm.homeautomation.entities.WindowBlind;
 
 @XmlRootElement
-public class WindowBlindStatus {
+public class WindowBlindStatus implements HumanMessageGenerationInterface {
 	private WindowBlind windowBlind;
 
 	public WindowBlind getWindowBlind() {
@@ -14,6 +15,14 @@ public class WindowBlindStatus {
 
 	public void setWindowBlind(WindowBlind windowBlind) {
 		this.windowBlind = windowBlind;
+	}
+
+	@Override
+	public String getMessageString() {
+		if (this.windowBlind != null) {
+			return "Window blind: "+this.windowBlind.getName()+" new status: "+ this.windowBlind.getCurrentValue();
+		}
+		return null;
 	}
 
 }
