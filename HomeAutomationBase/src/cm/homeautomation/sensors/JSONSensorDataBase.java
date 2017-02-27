838bd343e82481d5f9b88617f98c3544ef0984e8
Christoph Mertins <c.mertins@gmail.com>
2017-02-27 09:33:05 +0100

    change dependencies, add JSONInclude

--- HomeAutomationBase/src/cm/homeautomation/sensors/JSONSensorDataBase.java
+++ HomeAutomationBase/src/cm/homeautomation/sensors/JSONSensorDataBase.java
@@ -2,12 +2,14 @@ package cm.homeautomation.sensors;
 
 import javax.xml.bind.annotation.XmlRootElement;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonSubTypes.Type;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 
 @JsonTypeInfo(use=JsonTypeInfo.Id.MINIMAL_CLASS, include=JsonTypeInfo.As.PROPERTY, property="@c")
 @JsonSubTypes({@Type(value=SensorDataRoomSaveRequest.class), @Type(value=SensorDataSaveRequest.class), @Type(value=WindowSensorData.class)})
+@JsonInclude (JsonInclude.Include.NON_EMPTY)
 @XmlRootElement
 public abstract class JSONSensorDataBase {
 
