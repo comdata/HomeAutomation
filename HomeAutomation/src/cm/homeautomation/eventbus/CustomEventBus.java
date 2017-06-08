607fc9b9f8416275b69b80002d9211bd32a2545e
Christoph Mertins <c.mertins@gmail.com>
2017-06-08 12:40:56 +0200

    change class handling

--- HomeAutomation/src/cm/homeautomation/eventbus/CustomEventBus.java
+++ HomeAutomation/src/cm/homeautomation/eventbus/CustomEventBus.java
@@ -1,6 +1,8 @@
 package cm.homeautomation.eventbus;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
@@ -10,34 +12,34 @@ import com.google.common.eventbus.EventBus;
 
 public class CustomEventBus extends AsyncEventBus {
 
-	Set<Class> classes=new HashSet<Class>();
-	
+	Map<String, Class> classes = new HashMap<String, Class>();
+
 	EventBus eventBus;
-	
+
 	public CustomEventBus() {
 		super(Executors.newCachedThreadPool());
-		eventBus=this;
+		eventBus = this;
 	}
 
-	
 	@Override
 	public void register(Object object) {
-		
+
 		Class clazz = object.getClass();
-		
-		if(classes.contains(clazz)) {
-			System.out.println("Class already registered on eventbus: "+clazz.getName());
+		String clazzName = clazz.getName();
+
+		if (classes.containsKey(clazzName)) {
+			System.out.println("Class already registered on eventbus: " + clazzName);
 		} else {
-			System.out.println("Registering Class on eventbus: "+clazz.getName());
+			System.out.println("Registering Class on eventbus: " + clazz.getName());
 			eventBus.register(object);
-			classes.add(clazz);
+			classes.put(clazzName, clazz);
 		}
 	}
+
 	@Override
 	public void unregister(Object object) {
 		classes.remove(object.getClass());
 		eventBus.unregister(object);
 	}
-	
-	
+
 }
