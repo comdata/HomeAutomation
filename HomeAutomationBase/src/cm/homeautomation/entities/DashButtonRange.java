ab8b403f8e819282a418b08c3e1c24d99a9d7233
Christoph Mertins <c.mertins@gmail.com>
2017-09-17 17:27:33 +0200

    change column definition

--- HomeAutomationBase/src/cm/homeautomation/entities/DashButtonRange.java
+++ HomeAutomationBase/src/cm/homeautomation/entities/DashButtonRange.java
@@ -1,11 +1,13 @@
 package cm.homeautomation.entities;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
 @Entity
 public class DashButtonRange {
 	@Id
+	@Column(name="VENDORRANGE")
 	private String range=null;
 	
 	public void setRange(String range) {
