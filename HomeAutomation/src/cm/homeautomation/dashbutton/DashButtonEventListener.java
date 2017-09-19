b7e4c8c8c19cdb0156bc6bd9e8ce923a328a9f7d
Christoph Mertins <c.mertins@gmail.com>
2017-09-19 21:25:49 +0200

    toggle referenced switch

--- HomeAutomation/src/cm/homeautomation/dashbutton/DashButtonEventListener.java
+++ HomeAutomation/src/cm/homeautomation/dashbutton/DashButtonEventListener.java
@@ -8,7 +8,9 @@ import com.google.common.eventbus.Subscribe;
 
 import cm.homeautomation.db.EntityManagerService;
 import cm.homeautomation.entities.DashButton;
+import cm.homeautomation.entities.Switch;
 import cm.homeautomation.eventbus.EventObject;
+import cm.homeautomation.services.actor.ActorService;
 
 public class DashButtonEventListener {
 
@@ -24,16 +26,38 @@ public class DashButtonEventListener {
 			
 			String mac = dbEvent.getMac();
 			
-			List resultList = em.createQuery("select db from DashButton db where db.mac=:mac").setParameter("mac", mac).getResultList();
+			List<DashButton> resultList = (List<DashButton>)em.createQuery("select db from DashButton db where db.mac=:mac").setParameter("mac", mac).getResultList();
 			
+			DashButton dashButton=null;
 			if (resultList==null) {
 				em.getTransaction().begin();
 
-				DashButton dashButton = new DashButton();
+				dashButton = new DashButton();
 				dashButton.setMac(mac);
 				em.persist(dashButton);
 				em.getTransaction().commit();
+			} else {
+				for (DashButton db : resultList) {
+					dashButton=db;
+					break;
+				}
 			}
+			
+			if (dashButton!=null) {
+				Switch referencedSwitch = dashButton.getReferencedSwitch();
+				
+				if (referencedSwitch!=null) {
+					if (referencedSwitch!=null) {
+						String latestStatus = referencedSwitch.getLatestStatus();
+						
+						String newStatus=("ON".equals(latestStatus)?"OFF": "ON");
+						
+						ActorService.getInstance().pressSwitch(referencedSwitch.getId().toString(), newStatus);
+					}
+				}
+			}
+			
+
 		}
 	}
 	
