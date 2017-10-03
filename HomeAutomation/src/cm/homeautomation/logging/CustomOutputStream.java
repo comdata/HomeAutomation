6f0d7ab89485c33f8273352fff57363dabbd2530
Christoph Mertins <c.mertins@gmail.com>
2017-10-03 19:02:25 +0200

    change logging

--- HomeAutomation/src/cm/homeautomation/logging/CustomOutputStream.java
+++ HomeAutomation/src/cm/homeautomation/logging/CustomOutputStream.java
@@ -4,28 +4,47 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.io.PrintStream;
 
+import org.apache.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
 
 import cm.homeautomation.services.base.AutoCreateInstance;
 
 @AutoCreateInstance
 public class CustomOutputStream extends OutputStream {
-	
+
 	private static CustomOutputStream instance;
+	private Logger logger = Logger.getLogger(this.getClass());
 
 	public CustomOutputStream() {
 		System.setOut(new PrintStream(this));
-		instance=this;
+		instance = this;
 	}
-	 
+
 	@Override
 	public final void write(int b) throws IOException {
 		// the correct way of doing this would be using a buffer
 		// to store characters until a newline is encountered,
 		// this implementation is for illustration only
-		LogManager.getLogger(this.getClass()).info((char) b);
+		logger.info((char) b);
 	}
-	
+
+	@Override
+	public void write(byte[] b, int off, int len) throws IOException {
+		if (b == null) {
+			throw new NullPointerException();
+		} else if ((off < 0) || (off > b.length) || (len < 0) || ((off + len) > b.length) || ((off + len) < 0)) {
+			throw new IndexOutOfBoundsException();
+		} else if (len == 0) {
+			return;
+		}
+		byte[] pb = new byte[len];
+		for (int i = 0; i < len; i++) {
+			pb[i] = (b[off + i]);
+		}
+		String str = new String(pb);
+		logger.info(str);
+	}
+
 	public static CustomOutputStream getInstance() {
 		return instance;
 	}
