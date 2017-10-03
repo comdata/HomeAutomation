57d41877a0aa16b810b6c377517b9f5995e87c8a
Christoph Mertins <c.mertins@gmail.com>
2017-10-03 09:29:55 +0200

    minimum and maximum values configurable

--- HomeAutomationBase/src/cm/homeautomation/entities/DimmableLight.java
+++ HomeAutomationBase/src/cm/homeautomation/entities/DimmableLight.java
@@ -10,20 +10,22 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 
-@JsonTypeInfo(use=JsonTypeInfo.Id.NAME, include=JsonTypeInfo.As.PROPERTY, property="@class")
-@JsonSubTypes({
-    @JsonSubTypes.Type(value = RGBLight.class, name = "RGBLight")
-})
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "@class")
+@JsonSubTypes({ @JsonSubTypes.Type(value = RGBLight.class, name = "RGBLight") })
 @Entity
 public class DimmableLight extends Light {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
 
-	
-	@Column(name="BRIGHTNESS")
-	private int brightnessLevel;
+	@Column(name = "BRIGHTNESS")
+	private int brightnessLevel = 0;
+
+	@Column(name = "MINIMUM_VALUE")
+	private int minimumValue = 0;
+
+	@Column(name = "MAXIMUM_VALUE")
+	private int maximumValue = 0;
 
 	public int getBrightnessLevel() {
 		return brightnessLevel;
@@ -33,8 +35,20 @@ public class DimmableLight extends Light {
 		this.brightnessLevel = brightnessLevel;
 	}
 
+	public int getMinimumValue() {
+		return minimumValue;
+	}
 
+	public void setMinimumValue(int minimumValue) {
+		this.minimumValue = minimumValue;
+	}
 
-	
-	
+	public int getMaximumValue() {
+		return maximumValue;
+	}
+
+	public void setMaximumValue(int maximumValue) {
+		this.maximumValue = maximumValue;
+	}
+
 }
