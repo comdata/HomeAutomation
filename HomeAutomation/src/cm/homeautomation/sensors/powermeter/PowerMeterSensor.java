9014d8e53b2c3f0db5771d041a24a6736c38666f
Christoph Mertins <c.mertins@gmail.com>
2017-10-17 23:29:52 +0200

    add rate limiting to powermeter sensor

--- HomeAutomation/src/cm/homeautomation/sensors/powermeter/PowerMeterSensor.java
+++ HomeAutomation/src/cm/homeautomation/sensors/powermeter/PowerMeterSensor.java
@@ -1,7 +1,15 @@
 package cm.homeautomation.sensors.powermeter;
 
 import java.math.BigDecimal;
+
+import es.moki.ratelimitj.core.limiter.request.RequestLimitRule;
+import es.moki.ratelimitj.core.limiter.request.RequestRateLimiter;
+import es.moki.ratelimitj.inmemory.request.InMemorySlidingWindowRequestRateLimiter;
+
+import java.util.Collections;
 import java.util.Date;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
 import javax.persistence.EntityManager;
 
@@ -23,7 +31,7 @@ import cm.homeautomation.sensors.PowerMeterData;
 public class PowerMeterSensor {
 
 	public PowerMeterSensor() {
-		
+
 		EventBusService.getEventBus().register(this);
 	}
 
@@ -39,7 +47,7 @@ public class PowerMeterSensor {
 		Object data = eventObject.getData();
 		if (data instanceof PowerMeterData) {
 			EntityManager em = EntityManagerService.getNewManager();
-			
+
 			PowerMeterData powerData = (PowerMeterData) data;
 
 			PowerMeterPing powerMeterPing = new PowerMeterPing();
@@ -52,88 +60,97 @@ public class PowerMeterSensor {
 			em.persist(powerMeterPing);
 			em.getTransaction().commit();
 			em.close();
-			return;
-			
-			/*em = EntityManagerService.getNewManager();
 
-			BigDecimal oneMinute = (BigDecimal) em
-					.createNativeQuery(
-							"select count(*)/1000*60 from POWERMETERPING where TIMESTAMP >= now() - INTERVAL 1 MINUTE;")
-					.getSingleResult();
-			
-			BigDecimal oneMinuteTrend = (BigDecimal) em
-					.createNativeQuery(
-							"select count(*)/1000*60 from POWERMETERPING where TIMESTAMP >= now() - INTERVAL 2 MINUTE and TIMESTAMP <= now() - INTERVAL 1 MINUTE;")
-					.getSingleResult();
-			
-			BigDecimal fiveMinute = (BigDecimal) em
-					.createNativeQuery(
-							"select count(*)/1000*12 from POWERMETERPING where TIMESTAMP >= now() - INTERVAL 5 MINUTE;")
-					.getSingleResult();
-			
-			BigDecimal fiveMinuteTrend = (BigDecimal) em
-					.createNativeQuery(
-							"select count(*)/1000*12 from POWERMETERPING where TIMESTAMP >= now() - INTERVAL 6 MINUTE and TIMESTAMP <= now() - INTERVAL 1 MINUTE;")
-					.getSingleResult();
-			
-			BigDecimal sixtyMinute = (BigDecimal) em
-					.createNativeQuery(
-							"select count(*)/1000 from POWERMETERPING where TIMESTAMP >= now() - INTERVAL 60 MINUTE;")
-					.getSingleResult();
-			
-			BigDecimal sixtyMinuteTrend = (BigDecimal) em
-					.createNativeQuery(
-							"select count(*)/1000 from POWERMETERPING where TIMESTAMP >= now() - INTERVAL 61 MINUTE and TIMESTAMP <= now() - INTERVAL 1 MINUTE;;")
-					.getSingleResult();
+			Set<RequestLimitRule> rules = Collections.singleton(RequestLimitRule.of(1, TimeUnit.MINUTES, 2)); // 2
+																												// request
+																												// per
+																												// minute,
+																												// per
+																												// key
+			RequestRateLimiter requestRateLimiter = new InMemorySlidingWindowRequestRateLimiter(rules);
 
-			BigDecimal today = (BigDecimal) em
-					.createNativeQuery("select count(*)/1000 from POWERMETERPING where date(TIMESTAMP)=CURDATE() ;")
-					.getSingleResult();
-
-			
-			BigDecimal yesterday = (BigDecimal) em
-					.createNativeQuery("select count(*)/1000 from POWERMETERPING where date(TIMESTAMP)=date(now()- interval 1 day);")
-					.getSingleResult();
-
-			BigDecimal lastSevenDays = (BigDecimal) em
-					.createNativeQuery("select count(*)/1000 from POWERMETERPING where date(TIMESTAMP)>=date(now()- interval 8 day) and date(TIMESTAMP)<=date(now()- interval 1 day);")
-					.getSingleResult();
-			
-			BigDecimal lastEightDaysBeforeTillYesterday = (BigDecimal) em
-					.createNativeQuery("select count(*)/1000 from POWERMETERPING where date(TIMESTAMP)>=date(now()- interval 8 day) and date(TIMESTAMP)<CURDATE();")
-					.getSingleResult();
-			
-			oneMinute=oneMinute.setScale(2, BigDecimal.ROUND_HALF_UP);
-			oneMinuteTrend=oneMinuteTrend.setScale(2, BigDecimal.ROUND_HALF_UP);
-			fiveMinute=fiveMinute.setScale(2, BigDecimal.ROUND_HALF_UP);
-			fiveMinuteTrend=fiveMinuteTrend.setScale(2, BigDecimal.ROUND_HALF_UP);
-			sixtyMinute=sixtyMinute.setScale(2, BigDecimal.ROUND_HALF_UP);
-			sixtyMinuteTrend=sixtyMinuteTrend.setScale(2, BigDecimal.ROUND_HALF_UP);
-			today=today.setScale(2, BigDecimal.ROUND_HALF_UP);
-			yesterday=yesterday.setScale(2, BigDecimal.ROUND_HALF_UP);
-			lastSevenDays=lastSevenDays.setScale(2, BigDecimal.ROUND_HALF_UP);
-			lastEightDaysBeforeTillYesterday=lastEightDaysBeforeTillYesterday.setScale(2, BigDecimal.ROUND_HALF_UP);
-			
-			
-			
-			PowerMeterIntervalData powerMeterIntervalData = new PowerMeterIntervalData();
-			powerMeterIntervalData.setOneMinute(oneMinute.floatValue());
-			powerMeterIntervalData.setOneMinuteTrend(oneMinute.compareTo(oneMinuteTrend));
-			powerMeterIntervalData.setFiveMinute(fiveMinute.floatValue());
-			powerMeterIntervalData.setFiveMinuteTrend(fiveMinute.compareTo(fiveMinuteTrend));
-			powerMeterIntervalData.setSixtyMinute(sixtyMinute.floatValue());
-			powerMeterIntervalData.setSixtyMinuteTrend(sixtyMinute.compareTo(sixtyMinuteTrend));
-			powerMeterIntervalData.setToday(today.floatValue());
-			powerMeterIntervalData.setYesterday(yesterday.floatValue());
-			powerMeterIntervalData.setLastSevenDays(lastSevenDays.floatValue());
-			powerMeterIntervalData.setLastSevenDaysTrend(lastSevenDays.compareTo(lastEightDaysBeforeTillYesterday));
-			
-			em.close();
-			
-			EventObject intervalEventObject = new EventObject(powerMeterIntervalData);
-			EventBusService.getEventBus().post(intervalEventObject);*/
+			boolean overLimit = requestRateLimiter.overLimitWhenIncremented(PowerMeterData.class.getName());
+			if (overLimit) {
+				sendNewData();
+			}
 
 		}
+	}
+
+	private void sendNewData() {
+		EntityManager em = EntityManagerService.getNewManager();
+
+		BigDecimal oneMinute = (BigDecimal) em
+				.createNativeQuery(
+						"select count(*)/1000*60 from POWERMETERPING where TIMESTAMP >= now() - INTERVAL 1 MINUTE;")
+				.getSingleResult();
+
+		BigDecimal oneMinuteTrend = (BigDecimal) em.createNativeQuery(
+				"select count(*)/1000*60 from POWERMETERPING where TIMESTAMP >= now() - INTERVAL 2 MINUTE and TIMESTAMP <= now() - INTERVAL 1 MINUTE;")
+				.getSingleResult();
+
+		BigDecimal fiveMinute = (BigDecimal) em
+				.createNativeQuery(
+						"select count(*)/1000*12 from POWERMETERPING where TIMESTAMP >= now() - INTERVAL 5 MINUTE;")
+				.getSingleResult();
+
+		BigDecimal fiveMinuteTrend = (BigDecimal) em.createNativeQuery(
+				"select count(*)/1000*12 from POWERMETERPING where TIMESTAMP >= now() - INTERVAL 6 MINUTE and TIMESTAMP <= now() - INTERVAL 1 MINUTE;")
+				.getSingleResult();
+
+		BigDecimal sixtyMinute = (BigDecimal) em
+				.createNativeQuery(
+						"select count(*)/1000 from POWERMETERPING where TIMESTAMP >= now() - INTERVAL 60 MINUTE;")
+				.getSingleResult();
+
+		BigDecimal sixtyMinuteTrend = (BigDecimal) em.createNativeQuery(
+				"select count(*)/1000 from POWERMETERPING where TIMESTAMP >= now() - INTERVAL 61 MINUTE and TIMESTAMP <= now() - INTERVAL 1 MINUTE;;")
+				.getSingleResult();
+
+		BigDecimal today = (BigDecimal) em
+				.createNativeQuery("select count(*)/1000 from POWERMETERPING where date(TIMESTAMP)=CURDATE() ;")
+				.getSingleResult();
+
+		BigDecimal yesterday = (BigDecimal) em
+				.createNativeQuery(
+						"select count(*)/1000 from POWERMETERPING where date(TIMESTAMP)=date(now()- interval 1 day);")
+				.getSingleResult();
+
+		BigDecimal lastSevenDays = (BigDecimal) em.createNativeQuery(
+				"select count(*)/1000 from POWERMETERPING where date(TIMESTAMP)>=date(now()- interval 8 day) and date(TIMESTAMP)<=date(now()- interval 1 day);")
+				.getSingleResult();
+
+		BigDecimal lastEightDaysBeforeTillYesterday = (BigDecimal) em.createNativeQuery(
+				"select count(*)/1000 from POWERMETERPING where date(TIMESTAMP)>=date(now()- interval 8 day) and date(TIMESTAMP)<CURDATE();")
+				.getSingleResult();
+
+		oneMinute = oneMinute.setScale(2, BigDecimal.ROUND_HALF_UP);
+		oneMinuteTrend = oneMinuteTrend.setScale(2, BigDecimal.ROUND_HALF_UP);
+		fiveMinute = fiveMinute.setScale(2, BigDecimal.ROUND_HALF_UP);
+		fiveMinuteTrend = fiveMinuteTrend.setScale(2, BigDecimal.ROUND_HALF_UP);
+		sixtyMinute = sixtyMinute.setScale(2, BigDecimal.ROUND_HALF_UP);
+		sixtyMinuteTrend = sixtyMinuteTrend.setScale(2, BigDecimal.ROUND_HALF_UP);
+		today = today.setScale(2, BigDecimal.ROUND_HALF_UP);
+		yesterday = yesterday.setScale(2, BigDecimal.ROUND_HALF_UP);
+		lastSevenDays = lastSevenDays.setScale(2, BigDecimal.ROUND_HALF_UP);
+		lastEightDaysBeforeTillYesterday = lastEightDaysBeforeTillYesterday.setScale(2, BigDecimal.ROUND_HALF_UP);
+
+		PowerMeterIntervalData powerMeterIntervalData = new PowerMeterIntervalData();
+		powerMeterIntervalData.setOneMinute(oneMinute.floatValue());
+		powerMeterIntervalData.setOneMinuteTrend(oneMinute.compareTo(oneMinuteTrend));
+		powerMeterIntervalData.setFiveMinute(fiveMinute.floatValue());
+		powerMeterIntervalData.setFiveMinuteTrend(fiveMinute.compareTo(fiveMinuteTrend));
+		powerMeterIntervalData.setSixtyMinute(sixtyMinute.floatValue());
+		powerMeterIntervalData.setSixtyMinuteTrend(sixtyMinute.compareTo(sixtyMinuteTrend));
+		powerMeterIntervalData.setToday(today.floatValue());
+		powerMeterIntervalData.setYesterday(yesterday.floatValue());
+		powerMeterIntervalData.setLastSevenDays(lastSevenDays.floatValue());
+		powerMeterIntervalData.setLastSevenDaysTrend(lastSevenDays.compareTo(lastEightDaysBeforeTillYesterday));
+
+		em.close();
+
+		EventObject intervalEventObject = new EventObject(powerMeterIntervalData);
+		EventBusService.getEventBus().post(intervalEventObject);
 	}
 
 }
