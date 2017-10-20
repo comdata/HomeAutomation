6a11e9e99aa65300511c8aaa01c54fe41668d558
Christoph Mertins <c.mertins@gmail.com>
2017-10-20 17:03:40 +0200

    compression service

--- HomeAutomation/src/cm/homeautomation/sensors/powermeter/PowerMeterSensor.java
+++ HomeAutomation/src/cm/homeautomation/sensors/powermeter/PowerMeterSensor.java
@@ -1,22 +1,28 @@
 package cm.homeautomation.sensors.powermeter;
 
 import java.math.BigDecimal;
+import java.sql.Timestamp;
 
 import es.moki.ratelimitj.core.limiter.request.RequestLimitRule;
 import es.moki.ratelimitj.core.limiter.request.RequestRateLimiter;
 import es.moki.ratelimitj.inmemory.request.InMemorySlidingWindowRequestRateLimiter;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Date;
+import java.util.List;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 import javax.persistence.EntityManager;
 
+import org.apache.logging.log4j.LogManager;
+
 import com.google.common.eventbus.AllowConcurrentEvents;
 import com.google.common.eventbus.Subscribe;
 
 import cm.homeautomation.db.EntityManagerService;
+import cm.homeautomation.entities.PowerIntervalData;
 import cm.homeautomation.entities.PowerMeterPing;
 import cm.homeautomation.eventbus.EventBusService;
 import cm.homeautomation.eventbus.EventObject;
@@ -33,7 +39,8 @@ public class PowerMeterSensor {
 	private RequestRateLimiter requestRateLimiter;
 
 	public PowerMeterSensor() {
-		Set<RequestLimitRule> rules = Collections.singleton(RequestLimitRule.of(1, TimeUnit.MINUTES, 1)); // 1 per minute
+		Set<RequestLimitRule> rules = Collections.singleton(RequestLimitRule.of(1, TimeUnit.MINUTES, 1)); // 1 per
+																											// minute
 		requestRateLimiter = new InMemorySlidingWindowRequestRateLimiter(rules);
 		EventBusService.getEventBus().register(this);
 	}
@@ -42,6 +49,50 @@ public class PowerMeterSensor {
 		requestRateLimiter = null;
 		EventBusService.getEventBus().unregister(this);
 
+	}
+
+	public static void compress(String[] args) {
+		if (args != null && args.length > 0) {
+
+			int numberOfEntriesToCompress = Integer.parseInt(args[0]);
+
+			EntityManager em = EntityManagerService.getNewManager();
+
+			em.getTransaction().begin();
+
+			@SuppressWarnings("unchecked")
+			List<Object[]> rawResultList = em.createNativeQuery(
+					"select sum(powerCounter) as counter, FROM_UNIXTIME(ROUND(UNIX_TIMESTAMP(TIMESTAMP)/("
+							+ " 60 * 60))*60 *60) as TIMESLICE, MIN(TIMESTAMP), MAX(TIMESTAMP) "
+							+ "from POWERMETERPING where compress=0 "
+							+ " and TIMESTAMP <= now() - INTERVAL 2 HOUR"
+							+ " GROUP BY ROUND(UNIX_TIMESTAMP(TIMESTAMP)/("
+							+ "60" + " * 60)) order by TIMESTAMP asc limit "+numberOfEntriesToCompress)
+					.getResultList();
+
+			
+			for (Object[] resultElement : rawResultList) {
+				PowerMeterPing compressPowerPing = new PowerMeterPing();
+				compressPowerPing.setTimestamp((Timestamp) resultElement[1]);
+				compressPowerPing.setCompressed(true);
+				compressPowerPing.setPowerCounter(((BigDecimal)resultElement[0]).intValue());
+				
+				System.out.println();
+				
+				String minTimestamp=(String) resultElement[2]; 
+				String maxTimestamp=(String) resultElement[3];
+				
+				LogManager.getLogger(PowerMeterSensor.class).error("Min Timestamp: "+minTimestamp+ " max timestamp:"+maxTimestamp);
+				
+				//em.persist(compressPowerPing);
+			}
+
+			em.getTransaction().commit();
+
+		} else {
+			LogManager.getLogger(PowerMeterSensor.class)
+					.error("arguments not specified correctly - expecting number of entries for first method call");
+		}
 	}
 
 	@Subscribe
@@ -67,7 +118,7 @@ public class PowerMeterSensor {
 
 			boolean overLimit = requestRateLimiter.overLimitWhenIncremented(PowerMeterData.class.getName());
 			if (overLimit) {
-				//sendNewData();
+				// sendNewData();
 			}
 
 		}
@@ -76,51 +127,46 @@ public class PowerMeterSensor {
 	private void sendNewData() {
 		EntityManager em = EntityManagerService.getNewManager();
 
-		BigDecimal oneMinute = (BigDecimal) em
-				.createNativeQuery(
-						"select sum(powerCounter)/1000*60 from POWERMETERPING where TIMESTAMP >= now() - INTERVAL 1 MINUTE;")
+		BigDecimal oneMinute = (BigDecimal) em.createNativeQuery(
+				"select sum(POWERCOUNTER)/1000*60 from POWERMETERPING where TIMESTAMP >= now() - INTERVAL 1 MINUTE;")
 				.getSingleResult();
 
 		BigDecimal oneMinuteTrend = (BigDecimal) em.createNativeQuery(
-				"select sum(powerCounter)/1000*60 from POWERMETERPING where TIMESTAMP >= now() - INTERVAL 2 MINUTE and TIMESTAMP <= now() - INTERVAL 1 MINUTE;")
+				"select sum(POWERCOUNTER)/1000*60 from POWERMETERPING where TIMESTAMP >= now() - INTERVAL 2 MINUTE and TIMESTAMP <= now() - INTERVAL 1 MINUTE;")
 				.getSingleResult();
 
-		BigDecimal fiveMinute = (BigDecimal) em
-				.createNativeQuery(
-						"select sum(powerCounter)/1000*12 from POWERMETERPING where TIMESTAMP >= now() - INTERVAL 5 MINUTE;")
+		BigDecimal fiveMinute = (BigDecimal) em.createNativeQuery(
+				"select sum(POWERCOUNTER)/1000*12 from POWERMETERPING where TIMESTAMP >= now() - INTERVAL 5 MINUTE;")
 				.getSingleResult();
 
 		BigDecimal fiveMinuteTrend = (BigDecimal) em.createNativeQuery(
-				"select sum(powerCounter)/1000*12 from POWERMETERPING where TIMESTAMP >= now() - INTERVAL 6 MINUTE and TIMESTAMP <= now() - INTERVAL 1 MINUTE;")
+				"select sum(POWERCOUNTER)/1000*12 from POWERMETERPING where TIMESTAMP >= now() - INTERVAL 6 MINUTE and TIMESTAMP <= now() - INTERVAL 1 MINUTE;")
 				.getSingleResult();
 
-		BigDecimal sixtyMinute = (BigDecimal) em
-				.createNativeQuery(
-						"select sum(powerCounter)/1000 from POWERMETERPING where TIMESTAMP >= now() - INTERVAL 60 MINUTE;")
+		BigDecimal sixtyMinute = (BigDecimal) em.createNativeQuery(
+				"select sum(POWERCOUNTER)/1000 from POWERMETERPING where TIMESTAMP >= now() - INTERVAL 60 MINUTE;")
 				.getSingleResult();
 
 		BigDecimal sixtyMinuteTrend = (BigDecimal) em.createNativeQuery(
-				"select sum(powerCounter)/1000 from POWERMETERPING where TIMESTAMP >= now() - INTERVAL 61 MINUTE and TIMESTAMP <= now() - INTERVAL 1 MINUTE;;")
+				"select sum(POWERCOUNTER)/1000 from POWERMETERPING where TIMESTAMP >= now() - INTERVAL 61 MINUTE and TIMESTAMP <= now() - INTERVAL 1 MINUTE;;")
 				.getSingleResult();
 
 		BigDecimal today = (BigDecimal) em
-				.createNativeQuery("select sum(powerCounter)/1000 from POWERMETERPING where date(TIMESTAMP)=CURDATE() ;")
+				.createNativeQuery(
+						"select sum(POWERCOUNTER)/1000 from POWERMETERPING where date(TIMESTAMP)=CURDATE() ;")
 				.getSingleResult();
 
-		BigDecimal yesterday = (BigDecimal) em
-				.createNativeQuery(
-						"select sum(powerCounter)/1000 from POWERMETERPING where date(TIMESTAMP)=date(now()- interval 1 day);")
+		BigDecimal yesterday = (BigDecimal) em.createNativeQuery(
+				"select sum(POWERCOUNTER)/1000 from POWERMETERPING where date(TIMESTAMP)=date(now()- interval 1 day);")
 				.getSingleResult();
 
 		BigDecimal lastSevenDays = (BigDecimal) em.createNativeQuery(
-				"select sum(powerCounter)/1000 from POWERMETERPING where date(TIMESTAMP)>=date(now()- interval 8 day) and date(TIMESTAMP)<=date(now()- interval 1 day);")
+				"select sum(POWERCOUNTER)/1000 from POWERMETERPING where date(TIMESTAMP)>=date(now()- interval 8 day) and date(TIMESTAMP)<=date(now()- interval 1 day);")
 				.getSingleResult();
 
 		BigDecimal lastEightDaysBeforeTillYesterday = (BigDecimal) em.createNativeQuery(
-				"select sum(powerCounter)/1000 from POWERMETERPING where date(TIMESTAMP)>=date(now()- interval 8 day) and date(TIMESTAMP)<CURDATE();")
+				"select sum(POWERCOUNTER)/1000 from POWERMETERPING where date(TIMESTAMP)>=date(now()- interval 8 day) and date(TIMESTAMP)<CURDATE();")
 				.getSingleResult();
-		
-		
 
 		oneMinute = oneMinute.setScale(2, BigDecimal.ROUND_HALF_UP);
 		oneMinuteTrend = oneMinuteTrend.setScale(2, BigDecimal.ROUND_HALF_UP);
@@ -144,10 +190,9 @@ public class PowerMeterSensor {
 		powerMeterIntervalData.setYesterday(yesterday.floatValue());
 		powerMeterIntervalData.setLastSevenDays(lastSevenDays.floatValue());
 		powerMeterIntervalData.setLastSevenDaysTrend(lastSevenDays.compareTo(lastEightDaysBeforeTillYesterday));
-		
+
 		em.clear();
 		em.close();
-
 
 		EventObject intervalEventObject = new EventObject(powerMeterIntervalData);
 		EventBusService.getEventBus().post(intervalEventObject);
