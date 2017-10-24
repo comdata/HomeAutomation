485843a8627237d67fdee75568a793a0967f4473
Christoph Mertins <c.mertins@gmail.com>
2017-10-24 20:04:00 +0200

    IR Command Entity added

---
+++ HomeAutomationBase/src/cm/homeautomation/entities/IRCommand.java
@@ -0,0 +1,89 @@
+package cm.homeautomation.entities;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class IRCommand {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String type;
+	private String typeClear;
+	private int address;
+	private int command;
+	private List<Integer> values;
+	private String name;
+	private String description;
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getTypeClear() {
+		return typeClear;
+	}
+
+	public void setTypeClear(String typeClear) {
+		this.typeClear = typeClear;
+	}
+
+	public int getAddress() {
+		return address;
+	}
+
+	public void setAddress(int address) {
+		this.address = address;
+	}
+
+	public int getCommand() {
+		return command;
+	}
+
+	public void setCommand(int command) {
+		this.command = command;
+	}
+
+	public List<Integer> getValues() {
+		return values;
+	}
+
+	public void setValues(List<Integer> values) {
+		this.values = values;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	
+}
