c5c20c697f7d4791b7febc44ea4c5fe445d5f32c
Christoph Mertins <c.mertins@gmail.com>
2017-10-24 10:26:59 +0200

    add index to LOGGING table

--- HomeAutomationBase/src/cm/homeautomation/entities/LoggingEntity.java
+++ HomeAutomationBase/src/cm/homeautomation/entities/LoggingEntity.java
@@ -8,9 +8,11 @@ import javax.persistence.Table;
 
 import org.apache.logging.log4j.core.LogEvent;
 import org.apache.logging.log4j.core.appender.db.jpa.BasicLogEventEntity;
+import org.eclipse.persistence.annotations.Index;
 
 @Entity
 @Table(name="LOGGING")
+@Index(name = "ix_timemillis", columnNames = { "timeMillis" })
 public class LoggingEntity extends BasicLogEventEntity {
 
 	/**
