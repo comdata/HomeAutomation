d4d2cfde394c6156792b4708c0cb3ec309e1620f
Christoph Mertins <c.mertins@gmail.com>
2017-10-31 14:22:19 +0100

    convert to lower case

--- HomeAutomation/src/cm/homeautomation/device/DeviceService.java
+++ HomeAutomation/src/cm/homeautomation/device/DeviceService.java
@@ -13,52 +13,54 @@ import cm.homeautomation.entities.Room;
 
 public class DeviceService {
 
-	public static Long getRoomIdForMac(String mac) {
-		Long roomId = null;
-
-		Room room = getRoomForMac(mac);
-		if (room != null) {
-			roomId = room.getId();
-		}
-		return roomId;
-	}
-
-	public static Room getRoomForMac(String mac) {
-		try {
-			Device device = getDeviceForMac(mac);
-
-			if (device==null) {
-				return null;
-			}
-			
-			return device.getRoom();
-		} catch (NoResultException e) {
-			LogManager.getLogger(DeviceService.class).info("Mac: " + mac, e);
-
-			return null;
-		}
-	}
-
 	public static Device getDeviceForMac(String mac) {
 		try {
-			EntityManager em = EntityManagerService.getNewManager();
+			final EntityManager em = EntityManagerService.getNewManager();
+			mac = mac.toLowerCase();
 			LogManager.getLogger(DeviceService.class).info("Mac: " + mac);
 			@SuppressWarnings("unchecked")
-			List<Device> devices = (List<Device>) em.createQuery("select d from Device d where d.mac=:mac").setParameter("mac", mac)
-					.getResultList();
+			final List<Device> devices = em.createQuery("select d from Device d where d.mac=:mac")
+					.setParameter("mac", mac).getResultList();
 
 			em.close();
-			
-			if (devices != null && devices.isEmpty()) {
-				for (Device device : devices) {
+
+			if ((devices != null) && devices.isEmpty()) {
+				for (final Device device : devices) {
 					return device;
 				}
 			}
 			return null;
-		} catch (NoResultException e) {
-			LogManager.getLogger(DeviceService.class).info("Mac: " + mac, e);
+		} catch (final NoResultException e) {
+			LogManager.getLogger(DeviceService.class).error("Mac: " + mac, e);
 			return null;
 		}
+	}
+
+	public static Room getRoomForMac(final String mac) {
+		try {
+			final Device device = getDeviceForMac(mac);
+
+			if (device == null) {
+				LogManager.getLogger(DeviceService.class).error("No Device for mac found: " + mac);
+				return null;
+			}
+
+			return device.getRoom();
+		} catch (final NoResultException e) {
+			LogManager.getLogger(DeviceService.class).info("Mac: " + mac, e);
+
+			return null;
+		}
+	}
+
+	public static Long getRoomIdForMac(final String mac) {
+		Long roomId = null;
+
+		final Room room = getRoomForMac(mac);
+		if (room != null) {
+			roomId = room.getId();
+		}
+		return roomId;
 	}
 
 }
