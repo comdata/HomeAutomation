21453292060580fd5f96ea346ddd74460556e867
Christoph Mertins <c.mertins@gmail.com>
2017-11-12 18:33:43 +0100

    ignore unknown properties

--- HomeAutomationBase/src/cm/homeautomation/sensors/JSONSensorDataBase.java
+++ HomeAutomationBase/src/cm/homeautomation/sensors/JSONSensorDataBase.java
@@ -2,14 +2,18 @@ package cm.homeautomation.sensors;
 
 import javax.xml.bind.annotation.XmlRootElement;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonSubTypes.Type;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 
-@JsonTypeInfo(use=JsonTypeInfo.Id.MINIMAL_CLASS, include=JsonTypeInfo.As.PROPERTY, property="@c")
-@JsonSubTypes({@Type(value=IRData.class),@Type(value=AliveMessage.class),@Type(value=SensorDataRoomSaveRequest.class), @Type(value=SensorDataSaveRequest.class), @Type(value=WindowSensorData.class)})
-@JsonInclude (JsonInclude.Include.USE_DEFAULTS)
+@JsonTypeInfo(use = JsonTypeInfo.Id.MINIMAL_CLASS, include = JsonTypeInfo.As.PROPERTY, property = "@c")
+@JsonSubTypes({ @Type(value = IRData.class), @Type(value = AliveMessage.class),
+		@Type(value = SensorDataRoomSaveRequest.class), @Type(value = SensorDataSaveRequest.class),
+		@Type(value = WindowSensorData.class) })
+@JsonInclude(JsonInclude.Include.USE_DEFAULTS)
+@JsonIgnoreProperties(ignoreUnknown = true)
 @XmlRootElement
 public abstract class JSONSensorDataBase {
 
