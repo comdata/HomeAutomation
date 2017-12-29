536d3d98148fbf152e236369ec25a314cca9db9c
Christoph Mertins <c.mertins@gmail.com>
2017-12-29 12:51:26 +0100

    change database updater code

--- HomeAutomation/src/cm/homeautomation/networkMonitor/NetworkDeviceDatabaseUpdater.java
+++ HomeAutomation/src/cm/homeautomation/networkMonitor/NetworkDeviceDatabaseUpdater.java
@@ -4,7 +4,6 @@ import java.util.Date;
 import java.util.List;
 
 import javax.persistence.EntityManager;
-import javax.persistence.Query;
 
 import com.google.common.eventbus.Subscribe;
 
@@ -18,29 +17,33 @@ public class NetworkDeviceDatabaseUpdater {
 	public NetworkDeviceDatabaseUpdater() {
 		EventBusService.getEventBus().register(this);
 	}
-	
+
 	@Subscribe
 	public void handleNetworkDeviceFound(EventObject eventObject) {
-		Object data = eventObject.getData();
+		final Object data = eventObject.getData();
 		if (data instanceof NetworkScannerHostFoundMessage) {
-			NetworkScannerHostFoundMessage foundHostMessage=(NetworkScannerHostFoundMessage)data;
-			
-			NetworkDevice networkDevice = foundHostMessage.getHost();
-			
-			EntityManager em = EntityManagerService.getNewManager();
-			List<NetworkDevice> resultList; 
-			
-			String mac = networkDevice.getMac();
-			
-			if (mac!=null) {
-				resultList = em.createQuery("select n from NetworkDevice n where n.mac=:mac").setParameter("mac", mac).getResultList();
-			} else {
-				resultList = em.createQuery("select n from NetworkDevice n where n.ip=:ip").setParameter("ip", networkDevice.getIp()).getResultList();
+			final NetworkScannerHostFoundMessage foundHostMessage = (NetworkScannerHostFoundMessage) data;
+
+			final NetworkDevice networkDevice = foundHostMessage.getHost();
+
+			final EntityManager em = EntityManagerService.getNewManager();
+			List<NetworkDevice> resultList = null;
+
+			final String mac = networkDevice.getMac();
+
+			if (mac != null) {
+				resultList = em.createQuery("select n from NetworkDevice n where n.mac=:mac").setParameter("mac", mac)
+						.getResultList();
 			}
-			
-			if (resultList!=null && !resultList.isEmpty()) {
-				
-				NetworkDevice existingNetworkDevice = resultList.get(0);
+
+			if ((resultList == null) || resultList.isEmpty()) {
+				resultList = em.createQuery("select n from NetworkDevice n where n.ip=:ip")
+						.setParameter("ip", networkDevice.getIp()).getResultList();
+			}
+
+			if ((resultList != null) && !resultList.isEmpty()) {
+
+				final NetworkDevice existingNetworkDevice = resultList.get(0);
 				existingNetworkDevice.setIp(networkDevice.getIp());
 				existingNetworkDevice.setHostname(networkDevice.getHostname());
 				existingNetworkDevice.setMac(networkDevice.getMac());
@@ -50,16 +53,16 @@ public class NetworkDeviceDatabaseUpdater {
 				em.getTransaction().commit();
 			} else {
 				// this is a new device, so save it
-				
+
 				em.getTransaction().begin();
 				networkDevice.setLastSeen(new Date());
 				em.persist(networkDevice);
 				em.getTransaction().commit();
 			}
-			
+
 			em.close();
-			
+
 		}
 	}
-	
+
 }
