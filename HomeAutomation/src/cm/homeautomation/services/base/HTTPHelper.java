1477fb398e333cacbf4aa9c2b3d5eee0de125ec4
Christoph Mertins <c.mertins@gmail.com>
2018-01-12 10:20:30 +0100

    make http helper async

--- HomeAutomation/src/cm/homeautomation/services/base/HTTPHelper.java
+++ HomeAutomation/src/cm/homeautomation/services/base/HTTPHelper.java
@@ -2,6 +2,7 @@ package cm.homeautomation.services.base;
 
 import java.io.IOException;
 
+import org.apache.http.HttpResponse;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.CredentialsProvider;
@@ -9,28 +10,35 @@ import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.log4j.LogManager;
 
 public class HTTPHelper {
 	public static void performHTTPRequest(String url) {
-		try {
-			System.out.println("perform http call: "+url);
-			
-			HttpGet getMethod = new HttpGet(url);
-			HttpClientBuilder clientBuilder = HttpClientBuilder.create();
 
-			String[] userPassword = url.split("@")[0].replace("http://", "").split(":");
+		final Runnable httpRequestThread = () -> {
+			try {
+				LogManager.getLogger(HTTPHelper.class).debug("perform http call: " + url);
 
-			CredentialsProvider credsProvider = new BasicCredentialsProvider();
-			credsProvider.setCredentials(new AuthScope(getMethod.getURI().getHost(), getMethod.getURI().getPort()),
-					new UsernamePasswordCredentials(userPassword[0], userPassword[1]));
-			clientBuilder.setDefaultCredentialsProvider(credsProvider);
-			HttpClient httpClient = clientBuilder.build();
+				final HttpGet getMethod = new HttpGet(url);
+				final HttpClientBuilder clientBuilder = HttpClientBuilder.create();
 
-			httpClient.execute(getMethod);
+				final String[] userPassword = url.split("@")[0].replace("http://", "").split(":");
 
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+				final CredentialsProvider credsProvider = new BasicCredentialsProvider();
+				credsProvider.setCredentials(new AuthScope(getMethod.getURI().getHost(), getMethod.getURI().getPort()),
+						new UsernamePasswordCredentials(userPassword[0], userPassword[1]));
+				clientBuilder.setDefaultCredentialsProvider(credsProvider);
+				final HttpClient httpClient = clientBuilder.build();
+
+				final HttpResponse httpResponse = httpClient.execute(getMethod);
+
+				LogManager.getLogger(HTTPHelper.class).debug("http called done: " + httpResponse.getStatusLine());
+
+			} catch (final IOException e) {
+				LogManager.getLogger(HTTPHelper.class).error("calling URL: " + url + " failed", e);
+			}
+		};
+		new Thread(httpRequestThread).start();
+
 	}
 }
