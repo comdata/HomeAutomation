ab5021879782baee85413b4dc02a81c7a2ca3b59
Christoph Mertins <c.mertins@gmail.com>
2018-02-11 16:46:34 +0100

    add missing window

---
+++ HomeAutomationBase/src/cm/homeautomation/entities/Window.java
@@ -0,0 +1,54 @@
+package cm.homeautomation.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.JsonIdentityReference;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
+@Entity
+public class Window {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
+	@JsonIdentityReference(alwaysAsId = true)
+	@JsonBackReference("room")
+	@ManyToOne
+	@JoinColumn(name = "ROOM_ID")
+	private Room room;
+
+	private String name;
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public Room getRoom() {
+		return room;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setRoom(Room room) {
+		this.room = room;
+	}
+}
