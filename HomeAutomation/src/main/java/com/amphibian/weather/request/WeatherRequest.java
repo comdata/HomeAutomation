3ec446b9040347a6b2e8145a958df947666f88bb
Christoph Mertins <cmertins@t-h.de>
2018-03-12 21:52:28 +0100

    add cast

--- HomeAutomation/src/main/java/com/amphibian/weather/request/WeatherRequest.java
+++ HomeAutomation/src/main/java/com/amphibian/weather/request/WeatherRequest.java
@@ -10,93 +10,86 @@ import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-import org.glassfish.jersey.client.ClientResponse;
-
 import com.amphibian.weather.response.WeatherResponse;
-
 
 public class WeatherRequest {
 
 	private final static String BASE_URL = "http://api.wunderground.com/api/";
-	
-	private String apiKey;
-	private String zmw;
-	
-	private Set<Feature> features;
-	
-	public WeatherRequest() {
-		this.apiKey = null;
-		this.features = new HashSet<Feature>();
-	}
-	
-	public void setApiKey(String k) {
-		this.apiKey = k;
-	}
-	
-	public void addFeature(Feature f) {
-		this.features.add(f);
-	}
-	
-	public WeatherResponse query(String country, String city) {
-		
-		if (this.apiKey == null) {
-			return null;
-		}
-		
-		if (this.features.size() == 0) {
-			return null;
-		}
-		
-		Client c = ClientBuilder.newClient();
-	
-		
-		String url = BASE_URL + this.apiKey + "/";
-		Iterator<Feature> i = this.features.iterator();
-		while (i.hasNext()) {
-			url += i.next() + "/";
-		}
-		url += "/lang:DL/q/"; 
-				
-		if (zmw==null || "".equals(zmw)) {
-			url+=country +"/"+ city;
-		} else {
-			url+=zmw;
-		}
-		  
-				
-		url+=".json";
-		
-		System.out.println(url);
-		
-		WebTarget r = c.target(url);
-		Response response = r.request(MediaType.APPLICATION_JSON).get(); //.readEntity(ClientResponse.class);
-		WeatherResponse w = response.readEntity(WeatherResponse.class);
-		return w;
-		
-	}
-	
-	
+
 	public static void main(String[] args) {
 
-	/*	WeatherRequest req = new WeatherRequest();
-		req.setApiKey("your_api_key");
-		req.addFeature(Feature.CONDITIONS);
-		req.addFeature(Feature.FORECAST);
-		req.addFeature(Feature.ALERTS);
-		WeatherResponse resp = req.query("17084");
-		
-		System.out.println("Current weather: " + resp.getConditions().getWeather());
-		System.out.println("Current temp(F): " + resp.getConditions().getTempF());
-		*/
-		
+		/*
+		 * WeatherRequest req = new WeatherRequest(); req.setApiKey("your_api_key");
+		 * req.addFeature(Feature.CONDITIONS); req.addFeature(Feature.FORECAST);
+		 * req.addFeature(Feature.ALERTS); WeatherResponse resp = req.query("17084");
+		 * 
+		 * System.out.println("Current weather: " + resp.getConditions().getWeather());
+		 * System.out.println("Current temp(F): " + resp.getConditions().getTempF());
+		 */
+
+	}
+
+	private String apiKey;
+
+	private String zmw;
+
+	private final Set<Feature> features;
+
+	public WeatherRequest() {
+		this.apiKey = null;
+		this.features = new HashSet<>();
+	}
+
+	public void addFeature(Feature f) {
+		this.features.add(f);
 	}
 
 	public String getZmw() {
 		return zmw;
 	}
 
+	public WeatherResponse query(String country, String city) {
+
+		if (this.apiKey == null) {
+			return null;
+		}
+
+		if (this.features.size() == 0) {
+			return null;
+		}
+
+		final Client c = ClientBuilder.newClient();
+
+		String url = BASE_URL + this.apiKey + "/";
+		final Iterator<Feature> i = this.features.iterator();
+		while (i.hasNext()) {
+			url += i.next() + "/";
+		}
+		url += "/lang:DL/q/";
+
+		if ((zmw == null) || "".equals(zmw)) {
+			url += country + "/" + city;
+		} else {
+			url += zmw;
+		}
+
+		url += ".json";
+
+		System.out.println(url);
+
+		final WebTarget r = c.target(url);
+		final Response response = r.request(MediaType.APPLICATION_JSON).get(); // .readEntity(ClientResponse.class);
+		final WeatherResponse w = (WeatherResponse) response.getEntity();
+		return w;
+
+	}
+
+	public void setApiKey(String k) {
+		this.apiKey = k;
+	}
+
 	public void setZmw(String zmw) {
 		this.zmw = zmw;
 	}
-	
+
 }
