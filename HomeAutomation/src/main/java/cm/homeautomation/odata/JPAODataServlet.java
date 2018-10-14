fd3ec72fe2c68d7863711872b88b4d890f53b9bc
Christoph Mertins <c.mertins@gmail.com>
2018-10-14 22:36:56 +0200

    init JPA OData bridge on startup

--- HomeAutomation/src/main/java/cm/homeautomation/odata/JPAODataServlet.java
+++ HomeAutomation/src/main/java/cm/homeautomation/odata/JPAODataServlet.java
@@ -4,30 +4,18 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.io.UnsupportedEncodingException;
-import java.security.Principal;
-import java.sql.Connection;
-import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Enumeration;
 import java.util.HashMap;
-import java.util.Locale;
-import java.util.Map;
 
-import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.persistence.EntityManagerFactory;
-import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
-import javax.servlet.ServletInputStream;
-import javax.servlet.ServletOutputStream;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
+import org.apache.logging.log4j.LogManager;
 import org.apache.olingo.commons.api.edmx.EdmxReference;
 import org.apache.olingo.commons.api.ex.ODataException;
 import org.apache.olingo.commons.api.http.HttpMethod;
@@ -36,37 +24,43 @@ import org.apache.olingo.server.api.ODataHttpHandler;
 import org.apache.olingo.server.api.ODataRequest;
 import org.apache.olingo.server.api.ODataResponse;
 import org.apache.olingo.server.api.ServiceMetadata;
-import org.eclipse.persistence.internal.jpa.jdbc.DataSourceImpl;
-import org.flywaydb.core.internal.jdbc.DriverDataSource;
-import org.mariadb.jdbc.MariaDbPoolDataSource;
 
 import com.sap.olingo.jpa.metadata.api.JPAEdmProvider;
 import com.sap.olingo.jpa.metadata.api.JPAEntityManagerFactory;
-import com.sap.olingo.jpa.processor.core.api.JPAODataGetHandler;
 
-import cm.homeautomation.db.EntityManagerService;
+import cm.homeautomation.services.base.StartupAnnotationInitializer;
 
 public class JPAODataServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
 	private static final String PUNIT_NAME = "HA";
-	private static String[] packages = { "cm.homeautomation.entities" };
+	private static String[] packages = null; // { "cm.homeautomation.entities" };
+	private ODataHttpHandler handler;
 
 	@Override
-	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	public void init(ServletConfig config) throws ServletException {
 		try {
+			LogManager.getLogger(this.getClass()).info("JPA OData Bridge");
+
 			EntityManagerFactory emf = JPAEntityManagerFactory.getEntityManagerFactory(PUNIT_NAME,
 					new HashMap<String, Object>());
 			JPAEdmProvider metadataProvider = new JPAEdmProvider(PUNIT_NAME, emf, null, packages);
 			OData odata = OData.newInstance();
 			ServiceMetadata edm = odata.createServiceMetadata(metadataProvider, new ArrayList<EdmxReference>());
-			ODataHttpHandler handler = odata.createHandler(edm);
+			handler = odata.createHandler(edm);
+		} catch (ODataException e) {
+			throw new ServletException(e);
+		}
+	}
+
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		try {
+
 			handler.process(req, resp);
 		}
 
 		catch (RuntimeException e) {
-			throw new ServletException(e);
-		} catch (ODataException e) {
 			throw new ServletException(e);
 		}
 	}
@@ -74,17 +68,13 @@ public class JPAODataServlet extends HttpServlet {
 	public static void main(String[] args) throws NamingException {
 		try {
 
-			String url = "jdbc:mariadb://localhost:3306/HA?characterEncoding=utf8";
-
-			MariaDbPoolDataSource ds = new MariaDbPoolDataSource(url);
-			ds.setUser("root");
-
 			EntityManagerFactory emf = JPAEntityManagerFactory.getEntityManagerFactory(PUNIT_NAME,
 					new HashMap<String, Object>());
 			JPAEdmProvider metadataProvider = new JPAEdmProvider(PUNIT_NAME, emf, null, packages);
 
 			OData odata = OData.newInstance();
 			ServiceMetadata edm = odata.createServiceMetadata(metadataProvider, new ArrayList<EdmxReference>());
+
 			ODataHttpHandler handler = odata.createHandler(edm);
 			ODataRequest request = new ODataRequest();
 			request.setMethod(HttpMethod.GET);
@@ -104,9 +94,6 @@ public class JPAODataServlet extends HttpServlet {
 
 		} catch (ODataException e) {
 
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
