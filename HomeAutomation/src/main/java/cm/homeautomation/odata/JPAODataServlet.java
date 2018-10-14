d9952f03af26704598a6d440bbf43561fa8e21c6
Christoph Mertins <c.mertins@gmail.com>
2018-10-14 16:17:35 +0200

    change datasource

--- HomeAutomation/src/main/java/cm/homeautomation/odata/JPAODataServlet.java
+++ HomeAutomation/src/main/java/cm/homeautomation/odata/JPAODataServlet.java
@@ -8,6 +8,7 @@ import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
 import java.security.Principal;
 import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.HashMap;
@@ -38,6 +39,7 @@ import org.apache.olingo.server.api.ODataResponse;
 import org.apache.olingo.server.api.ServiceMetadata;
 import org.eclipse.persistence.internal.jpa.jdbc.DataSourceImpl;
 import org.flywaydb.core.internal.jdbc.DriverDataSource;
+import org.mariadb.jdbc.MariaDbPoolDataSource;
 
 import com.sap.olingo.jpa.metadata.api.JPAEdmProvider;
 import com.sap.olingo.jpa.processor.core.api.JPAODataGetHandler;
@@ -52,41 +54,48 @@ public class JPAODataServlet extends HttpServlet {
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		try {
-			DriverDataSource dataSourceImpl =new DriverDataSource(ClassLoader.getSystemClassLoader(), "org.mariadb.jdbc.Driver", "jdbc:mariadb://localhost:3306/HA?characterEncoding=utf8", "root", "");
+			String url = "jdbc:mariadb://localhost:3306/HA?characterEncoding=utf8";
 			
-			JPAODataGetHandler handler=new JPAODataGetHandler(PUNIT_NAME, dataSourceImpl);
+			MariaDbPoolDataSource ds = new MariaDbPoolDataSource(url);
+			ds.setUser("root");
+
+			JPAODataGetHandler handler = new JPAODataGetHandler(PUNIT_NAME, ds);
 
 			handler.process(req, resp);
-		} catch (RuntimeException e) {
+		} catch (SQLException e) {
+
+			throw new ServletException(e);
+		}
+
+		catch (RuntimeException e) {
 			throw new ServletException(e);
 		} catch (ODataException e) {
-		throw new ServletException(e);
+			throw new ServletException(e);
 		}
 	}
-	
+
 	public static void main(String[] args) throws NamingException {
 		try {
-			String[] packages={"cm.homeautomation.entities"};
+			String url = "jdbc:mariadb://localhost:3306/HA?characterEncoding=utf8";
 			
-			DriverDataSource dataSourceImpl =new DriverDataSource(ClassLoader.getSystemClassLoader(), "org.mariadb.jdbc.Driver", "jdbc:mariadb://localhost:3306/HA?characterEncoding=utf8", "root", "");
-			
-			JPAODataGetHandler handler=new JPAODataGetHandler(PUNIT_NAME, dataSourceImpl);
-			
-			HttpServletRequest request = new HttpServletRequest ()
-			{
-			    private final Map<String, String[]> params = new HashMap<>();
+			MariaDbPoolDataSource ds = new MariaDbPoolDataSource(url);
+			ds.setUser("root");
 
-			    public Map<String, String[]> getParameterMap()
-			    {
-			        return params;
-			    }
+			JPAODataGetHandler handler = new JPAODataGetHandler(PUNIT_NAME, ds);
 
-			    public String getParameter(String name)
-			    {
-			        String[] matches = params.get(name);
-			        if (matches == null || matches.length == 0) return null;
-			        return matches[0];
-			    }
+			HttpServletRequest request = new HttpServletRequest() {
+				private final Map<String, String[]> params = new HashMap<>();
+
+				public Map<String, String[]> getParameterMap() {
+					return params;
+				}
+
+				public String getParameter(String name) {
+					String[] matches = params.get(name);
+					if (matches == null || matches.length == 0)
+						return null;
+					return matches[0];
+				}
 
 				@Override
 				public Object getAttribute(String name) {
@@ -109,7 +118,7 @@ public class JPAODataServlet extends HttpServlet {
 				@Override
 				public void setCharacterEncoding(String env) throws UnsupportedEncodingException {
 					// TODO Auto-generated method stub
-					
+
 				}
 
 				@Override
@@ -187,13 +196,13 @@ public class JPAODataServlet extends HttpServlet {
 				@Override
 				public void setAttribute(String name, Object o) {
 					// TODO Auto-generated method stub
-					
+
 				}
 
 				@Override
 				public void removeAttribute(String name) {
 					// TODO Auto-generated method stub
-					
+
 				}
 
 				@Override
@@ -294,7 +303,7 @@ public class JPAODataServlet extends HttpServlet {
 
 				@Override
 				public String getMethod() {
-					
+
 					return "GET";
 				}
 
@@ -401,207 +410,210 @@ public class JPAODataServlet extends HttpServlet {
 				}
 
 			};
-			
-			HttpServletResponse response=new HttpServletResponse() {
-				
+
+			HttpServletResponse response = new HttpServletResponse() {
+
 				@Override
 				public void setLocale(Locale loc) {
 					// TODO Auto-generated method stub
-					
+
 				}
-				
+
 				@Override
 				public void setContentType(String type) {
 					// TODO Auto-generated method stub
-					
+
 				}
-				
+
 				@Override
 				public void setContentLength(int len) {
 					// TODO Auto-generated method stub
-					
+
 				}
-				
+
 				@Override
 				public void setCharacterEncoding(String charset) {
 					// TODO Auto-generated method stub
-					
+
 				}
-				
+
 				@Override
 				public void setBufferSize(int size) {
 					// TODO Auto-generated method stub
-					
+
 				}
-				
+
 				@Override
 				public void resetBuffer() {
 					// TODO Auto-generated method stub
-					
+
 				}
-				
+
 				@Override
 				public void reset() {
 					// TODO Auto-generated method stub
-					
+
 				}
-				
+
 				@Override
 				public boolean isCommitted() {
 					// TODO Auto-generated method stub
 					return false;
 				}
-				
+
 				@Override
 				public PrintWriter getWriter() throws IOException {
 					// TODO Auto-generated method stub
 					return null;
 				}
-				
+
 				@Override
 				public ServletOutputStream getOutputStream() throws IOException {
 					// TODO Auto-generated method stub
 					return null;
 				}
-				
+
 				@Override
 				public Locale getLocale() {
 					// TODO Auto-generated method stub
 					return null;
 				}
-				
+
 				@Override
 				public String getContentType() {
 					// TODO Auto-generated method stub
 					return null;
 				}
-				
+
 				@Override
 				public String getCharacterEncoding() {
 					// TODO Auto-generated method stub
 					return null;
 				}
-				
+
 				@Override
 				public int getBufferSize() {
 					// TODO Auto-generated method stub
 					return 0;
 				}
-				
+
 				@Override
 				public void flushBuffer() throws IOException {
 					// TODO Auto-generated method stub
-					
+
 				}
-				
+
 				@Override
 				public void setStatus(int sc, String sm) {
 					// TODO Auto-generated method stub
-					
+
 				}
-				
+
 				@Override
 				public void setStatus(int sc) {
 					// TODO Auto-generated method stub
-					
+
 				}
-				
+
 				@Override
 				public void setIntHeader(String name, int value) {
 					// TODO Auto-generated method stub
-					
+
 				}
-				
+
 				@Override
 				public void setHeader(String name, String value) {
 					// TODO Auto-generated method stub
-					
+
 				}
-				
+
 				@Override
 				public void setDateHeader(String name, long date) {
 					// TODO Auto-generated method stub
-					
+
 				}
-				
+
 				@Override
 				public void sendRedirect(String location) throws IOException {
 					// TODO Auto-generated method stub
-					
+
 				}
-				
+
 				@Override
 				public void sendError(int sc, String msg) throws IOException {
 					// TODO Auto-generated method stub
-					
+
 				}
-				
+
 				@Override
 				public void sendError(int sc) throws IOException {
 					// TODO Auto-generated method stub
-					
+
 				}
-				
+
 				@Override
 				public String encodeUrl(String url) {
 					// TODO Auto-generated method stub
 					return null;
 				}
-				
+
 				@Override
 				public String encodeURL(String url) {
 					// TODO Auto-generated method stub
 					return null;
 				}
-				
+
 				@Override
 				public String encodeRedirectUrl(String url) {
 					// TODO Auto-generated method stub
 					return null;
 				}
-				
+
 				@Override
 				public String encodeRedirectURL(String url) {
 					// TODO Auto-generated method stub
 					return null;
 				}
-				
+
 				@Override
 				public boolean containsHeader(String name) {
 					// TODO Auto-generated method stub
 					return false;
 				}
-				
+
 				@Override
 				public void addIntHeader(String name, int value) {
 					// TODO Auto-generated method stub
-					
+
 				}
-				
+
 				@Override
 				public void addHeader(String name, String value) {
 					// TODO Auto-generated method stub
-					
+
 				}
-				
+
 				@Override
 				public void addDateHeader(String name, long date) {
 					// TODO Auto-generated method stub
-					
+
 				}
-				
+
 				@Override
 				public void addCookie(Cookie cookie) {
 					// TODO Auto-generated method stub
-					
+
 				}
 			};
 			handler.process(request, response);
-			
+
 		} catch (RuntimeException e) {
 
 		} catch (ODataException e) {
-		
-		} 
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
