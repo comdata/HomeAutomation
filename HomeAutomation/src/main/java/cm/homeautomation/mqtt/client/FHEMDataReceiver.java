43f10afa0dfa5362db5f7a46f475ab8e8dfb5730
Christoph Mertins <c.mertins@gmail.com>
2018-10-18 20:22:49 +0200

    add more logic

--- HomeAutomation/src/main/java/cm/homeautomation/mqtt/client/FHEMDataReceiver.java
+++ HomeAutomation/src/main/java/cm/homeautomation/mqtt/client/FHEMDataReceiver.java
@@ -1,5 +1,15 @@
 package cm.homeautomation.mqtt.client;
 
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
+import org.apache.logging.log4j.LogManager;
+
+import cm.homeautomation.db.EntityManagerService;
+import cm.homeautomation.entities.FHEMDevice;
+import cm.homeautomation.entities.FHEMDevice.FHEMDeviceType;
+
 /**
  * receive and event stream from FHEM via MQTT
  * 
@@ -11,9 +21,60 @@ public class FHEMDataReceiver {
 	private FHEMDataReceiver() {
 		// do nothing
 	}
-	
+
 	public static void receiveFHEMData(String topic, String messageContent) {
-System.out.println("FHEM message for topic: "+topic+" message: "+messageContent);
+		System.out.println("FHEM message for topic: " + topic + " message: " + messageContent);
+
+		EntityManager em = EntityManagerService.getManager();
+
+		if (topic != null) {
+			String[] topicParts = topic.split("/");
+
+			if (topicParts != null && topicParts.length >= 3) {
+
+				String device = topicParts[1];
+
+				@SuppressWarnings("unchecked")
+				List<FHEMDevice> resultList = em.createQuery("select f from FHEMDevice f where f.name=:name")
+						.setParameter("name", device).getResultList();
+
+				if (resultList != null && !resultList.isEmpty()) {
+					for (FHEMDevice fhemDevice : resultList) {
+						FHEMDeviceType deviceType = fhemDevice.getDeviceType();
+
+						if (deviceType != null) {
+
+							switch (deviceType) {
+							case WINDOW:
+								LogManager.getLogger(FHEMDataReceiver.class).error("Add implementation for device: " + device);
+								break;
+							case SWITCH:
+								LogManager.getLogger(FHEMDataReceiver.class).error("Add implementation for device: " + device);
+								break;
+							case WINDOWBLIND:
+								LogManager.getLogger(FHEMDataReceiver.class).error("Add implementation for device: " + device);
+								break;
+							case DEVICE:
+								LogManager.getLogger(FHEMDataReceiver.class).error("Add implementation for device: " + device);
+								break;
+								
+							default:
+								LogManager.getLogger(FHEMDataReceiver.class).error(
+										"Device type: " + deviceType + " for device: " + device + " not mapped.");
+								break;
+							}
+
+						} else {
+							LogManager.getLogger(FHEMDataReceiver.class).error("Device type for device: " + device);
+						}
+					}
+				} else {
+					LogManager.getLogger(FHEMDataReceiver.class).error("FHEM Device not found for device: " + device);
+				}
+
+			}
+
+		}
 	}
 
 }
