32ebe3ef6683ec7092375b4cdea0c949b42e3326
Christoph Mertins <c.mertins@gmail.com>
2018-10-20 22:49:46 +0200

    add generic device handling

--- src/main/java/cm/homeautomation/mqtt/client/FHEMDeviceDataReceiver.java
+++ src/main/java/cm/homeautomation/mqtt/client/FHEMDeviceDataReceiver.java
@@ -17,42 +17,48 @@ public class FHEMDeviceDataReceiver {
 	private FHEMDeviceDataReceiver() {
 		// do not create an instance
 	}
-	
+
 	public static void receive(String topic, String messageContent, FHEMDevice fhemDevice) {
 
 		EntityManager em = EntityManagerService.getManager();
 
 		Device device = em.find(Device.class, fhemDevice.getReferencedId());
-		
+
 		String[] topicParts = topic.split("/");
 
-		String topicLastPart = topicParts[topicParts.length-1].toLowerCase();
+		String topicLastPart = topicParts[topicParts.length - 1].toLowerCase();
 
 		switch (topicLastPart) {
 		case "power":
 			Sensor powerSensor = findSensorForTopic(device, topicLastPart);
-			if ( powerSensor!=null) {
+			if (powerSensor != null) {
 				Sensors.getInstance().saveSensorData(powerSensor.getId(), messageContent.split(" ")[0]);
 			}
 			break;
 
 		default:
-			LogManager.getLogger(FHEMDataReceiver.class).error("Device: add handling for last part: " + topicLastPart);
+			Map<String, Sensor> sensors = device.getSensors();
+
+			Sensor sensor = sensors.get(topicLastPart);
+
+			if (sensor != null) {
+				Sensors.getInstance().saveSensorData(sensor.getId(), messageContent.split(" ")[0]);
+			}
 		}
 
 	}
 
 	private static Sensor findSensorForTopic(Device device, String topicLastPart) {
-		
+
 		Map<String, Sensor> sensors = device.getSensors();
 		for (String sensorKey : sensors.keySet()) {
-			Sensor sensor=sensors.get(sensorKey);
+			Sensor sensor = sensors.get(sensorKey);
 			if (sensor.getSensorName().equals(topicLastPart)) {
 				return sensor;
 			}
-			
+
 		}
-		
+
 		return null;
 	}
 
