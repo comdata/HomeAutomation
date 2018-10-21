9276257706416fe261acb89060b4d52eeb0d4fc4
Christoph Mertins <c.mertins@gmail.com>
2018-10-21 17:45:36 +0200

    Device data and sensor handling

--- src/main/java/cm/homeautomation/fhem/FHEMDeviceDataReceiver.java
+++ src/main/java/cm/homeautomation/fhem/FHEMDeviceDataReceiver.java
@@ -12,12 +12,25 @@ import cm.homeautomation.entities.FHEMDevice;
 import cm.homeautomation.entities.Sensor;
 import cm.homeautomation.services.sensors.Sensors;
 
+/**
+ * FHEM integration
+ * 
+ * @author cmertins
+ *
+ */
 public class FHEMDeviceDataReceiver {
 
 	private FHEMDeviceDataReceiver() {
 		// do not create an instance
 	}
 
+	/**
+	 * forward sensor data to the Sensors service for storing
+	 * 
+	 * @param topic
+	 * @param messageContent
+	 * @param fhemDevice
+	 */
 	public static void receive(String topic, String messageContent, FHEMDevice fhemDevice) {
 
 		EntityManager em = EntityManagerService.getManager();
@@ -32,15 +45,24 @@ public class FHEMDeviceDataReceiver {
 
 		if (sensor != null) {
 			Sensors.getInstance().saveSensorData(sensor.getId(), messageContent.split(" ")[0]);
+		} else {
+			LogManager.getLogger(FHEMDeviceDataReceiver.class).error("Message for device: "+device+" topic: "+topicLastPart+" message: "+messageContent
+					+" received. No Sensor attached.");
 		}
 
 	}
 
+	/**
+	 * find a sensor for a topic in the list
+	 * 
+	 * @param device
+	 * @param topicLastPart
+	 * @return
+	 */
 	public static Sensor getSensorForTopic(Device device, String topicLastPart) {
 		Map<String, Sensor> sensors = device.getSensors();
 
-		Sensor sensor = sensors.get(topicLastPart);
-		return sensor;
+		return sensors.get(topicLastPart);
 	}
 
 }
