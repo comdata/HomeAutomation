1eb0e0d5cdc73a3bf558662c48b934ea8a50e907
Christoph Mertins <c.mertins@gmail.com>
2018-11-01 11:50:33 +0100

    remove unused implementation code

--- src/main/java/cm/homeautomation/services/base/JerseyApplication.java
+++ src/main/java/cm/homeautomation/services/base/JerseyApplication.java
@@ -18,50 +18,10 @@ public class JerseyApplication extends Application {
 	public JerseyApplication() {
 		super();
 	}
-	
-	private void setupTestData() {
-		EntityManager em = EntityManagerService.getManager();
-
-		em.getTransaction().begin();
-		em.createQuery("delete from SensorData" ).executeUpdate();
-		em.createQuery("delete from Sensor" ).executeUpdate();
-		em.createQuery("delete from Room" ).executeUpdate();
-		
-		
-		em.getTransaction().commit();
-		em.getTransaction().begin();
-		
-		Room room = new Room();
-		
-		room.setRoomName("Wohnzimmer");
-		
-		Sensor tempSensor=new Sensor();
-		tempSensor.setSensorName("Temperature");
-		tempSensor.setSensorPin("18");
-		tempSensor.setSensorTechnicalType("11");
-		tempSensor.setSensorType("TEMPERATURE");
-		tempSensor.setSensorPosition("LOCAL");
-		tempSensor.setRoom(room);
-		
-		room.getSensors().add(tempSensor);
-		
-		Sensor humSensor=new Sensor();
-		humSensor.setSensorName("Humidity");
-		humSensor.setSensorPin("18");
-		humSensor.setSensorTechnicalType("11");
-		humSensor.setSensorType("HUMIDITY");
-		humSensor.setSensorPosition("LOCAL");
-		humSensor.setRoom(room);
-		
-		room.getSensors().add(humSensor);
-		
-		em.persist(room);
-		em.getTransaction().commit();
-	}
 
 	@Override
 	public Set<Class<?>> getClasses() {
-		final Set<Class<?>> classes = new HashSet<Class<?>>();
+		final Set<Class<?>> classes = new HashSet<>();
 		// register resources and features
 		classes.add(MultiPartFeature.class);
 		classes.add(JacksonFeature.class);
