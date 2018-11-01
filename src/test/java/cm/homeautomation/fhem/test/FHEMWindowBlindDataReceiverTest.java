09dc77c6386345e3c84801a6b479e402af1df44c
Christoph Mertins <c.mertins@gmail.com>
2018-11-01 11:53:51 +0100

    add room to test case

--- src/test/java/cm/homeautomation/fhem/test/FHEMWindowBlindDataReceiverTest.java
+++ src/test/java/cm/homeautomation/fhem/test/FHEMWindowBlindDataReceiverTest.java
@@ -4,17 +4,35 @@ import static org.junit.Assert.*;
 
 import javax.persistence.EntityManager;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import cm.homeautomation.db.EntityManagerService;
 import cm.homeautomation.entities.FHEMDevice;
+import cm.homeautomation.entities.Room;
 import cm.homeautomation.entities.FHEMDevice.FHEMDeviceType;
 import cm.homeautomation.entities.WindowBlind;
 import cm.homeautomation.fhem.FHEMWindowBlindDataReceiver;
 import cm.homeautomation.services.base.GenericStatus;
+import cm.homeautomation.services.sensors.Sensors;
 
 public class FHEMWindowBlindDataReceiverTest {
+	private EntityManager em;
+	private Room room;
+	
+	@Before
+	public void setup() {
+		em = EntityManagerService.getNewManager();
 
+		em.getTransaction().begin();
+		room = new Room();
+		room.setRoomName("FHEM Room "+System.currentTimeMillis());
+
+		em.persist(room);
+
+		em.getTransaction().commit();
+	}
+	
 	@Test
 	public void testReceiveSuccessful() {
 		EntityManager em = EntityManagerService.getNewManager();
@@ -23,12 +41,14 @@ public class FHEMWindowBlindDataReceiverTest {
 		
 		WindowBlind windowBlind = new WindowBlind();
 		windowBlind.setName(""+System.currentTimeMillis());
+		windowBlind.setRoom(room);
 		em.persist(windowBlind);
 		
 		FHEMDevice fhemDevice = new FHEMDevice();
 		fhemDevice.setDeviceType(FHEMDeviceType.WINDOWBLIND);
 		fhemDevice.setReferencedId(windowBlind.getId());
 		fhemDevice.setName(windowBlind.getName());
+		
 		
 		em.getTransaction().commit();
 		
@@ -46,6 +66,7 @@ public class FHEMWindowBlindDataReceiverTest {
 		
 		WindowBlind windowBlind = new WindowBlind();
 		windowBlind.setName(""+System.currentTimeMillis());
+		windowBlind.setRoom(room);
 		em.persist(windowBlind);
 		
 		FHEMDevice fhemDevice = new FHEMDevice();
