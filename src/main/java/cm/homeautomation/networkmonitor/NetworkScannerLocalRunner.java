d9d769d2d10219556b5aa607dd4aa7f49f9d7963
Christoph Mertins <c.mertins@gmail.com>
2018-11-11 22:22:28 +0100

    cleanup

--- src/main/java/cm/homeautomation/networkmonitor/NetworkScannerLocalRunner.java
+++ src/main/java/cm/homeautomation/networkmonitor/NetworkScannerLocalRunner.java
@@ -1,29 +1,26 @@
 package cm.homeautomation.networkmonitor;
 
+import org.apache.log4j.LogManager;
+
 import cm.homeautomation.services.base.EventBusAnnotationInitializer;
 
 public class NetworkScannerLocalRunner {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		EventBusAnnotationInitializer eventBusAnnotationInitializer = new EventBusAnnotationInitializer();
+		eventBusAnnotationInitializer.getInstances();
 		String subnet = "192.168.1";
-		String[] networks={subnet};
-		
-		//NetworkDeviceDatabaseUpdater networkDeviceDatabaseUpdater = new NetworkDeviceDatabaseUpdater();
 
 		try {
 			Thread.sleep(15000);
 		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			LogManager.getLogger(NetworkScannerLocalRunner.class).error(e);
+			throw e;
 		}
-		
-		System.out.println("Triggering scan");
-		
-		NetworkScanner.getInstance().checkHosts(subnet);
 
-		
-		
+		LogManager.getLogger(NetworkScannerLocalRunner.class).debug("Triggering scan");
+
+		NetworkScanner.getInstance().checkHosts(subnet);
 	}
 
 }
