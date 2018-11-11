841826bfb8179aa859d0fc9174a4582d4a55de9d
Christoph Mertins <c.mertins@gmail.com>
2018-11-11 22:07:28 +0100

    add tests

--- src/test/java/cm/homeautomation/windowblind/test/WindowBlindServiceTest.java
+++ src/test/java/cm/homeautomation/windowblind/test/WindowBlindServiceTest.java
@@ -24,17 +24,17 @@ public class WindowBlindServiceTest {
 	private Room roomOne;
 	private Room roomTwo;
 	private WindowBlind singleWindowBlindTwo;
+	private EntityManager em;
 
 	@BeforeEach
 	public void setup() {
 		windowBlindService = new WindowBlindService();
 
-		EntityManager em = EntityManagerService.getNewManager();
+		em = EntityManagerService.getNewManager();
 
 		em.getTransaction().begin();
 		// clear all
-		em.createQuery("delete from WindowBlind w").executeUpdate();
-		em.createQuery("delete from Room r where r.roomName like 'WindowBlind%'").executeUpdate();
+		deleteExistingEntries(em);
 
 		roomOne = new Room();
 		roomOne.setRoomName("WindowBlind " + System.currentTimeMillis());
@@ -66,6 +66,11 @@ public class WindowBlindServiceTest {
 		em.getTransaction().commit();
 	}
 
+	private void deleteExistingEntries(EntityManager em) {
+		em.createQuery("delete from WindowBlind w").executeUpdate();
+		em.createQuery("delete from Room r where r.roomName like 'WindowBlind%'").executeUpdate();
+	}
+
 	@Test
 	public void testGetAll() {
 		WindowBlindsList all = windowBlindService.getAll();
@@ -77,6 +82,19 @@ public class WindowBlindServiceTest {
 	}
 	
 	@Test
+	public void testGetAllNoWindowBlindAvailable() {
+		em.getTransaction().begin();
+		deleteExistingEntries(em);
+		em.getTransaction().commit();
+		
+		WindowBlindsList all = windowBlindService.getAll();
+		
+		assertNotNull(all);
+		assertNotNull(all.getWindowBlinds());
+		assertTrue(all.getWindowBlinds().isEmpty());
+	}
+	
+	@Test
 	public void testGetAllForRoom() {
 		WindowBlindsList allForRoom = windowBlindService.getAllForRoom(roomTwo.getId());
 		
@@ -85,5 +103,17 @@ public class WindowBlindServiceTest {
 		assertFalse(allForRoom.getWindowBlinds().isEmpty());
 		assertTrue(allForRoom.getWindowBlinds().size()==2);
 		assertTrue(allForRoom.getWindowBlinds().get(1).getType().equals(WindowBlind.ALL_AT_ONCE));
+	}
+	@Test
+	public void testGetAllForRoomNoWindowBlindAvailable() {
+		em.getTransaction().begin();
+		deleteExistingEntries(em);
+		em.getTransaction().commit();
+		
+		WindowBlindsList allForRoom = windowBlindService.getAllForRoom(roomTwo.getId());
+		
+		assertNotNull(allForRoom);
+		assertNotNull(allForRoom.getWindowBlinds());
+		assertTrue(allForRoom.getWindowBlinds().isEmpty());
 	}
 }
