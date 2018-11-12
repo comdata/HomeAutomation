cb0533fb0b0fc83cfb750c3b3c4a1fe7ceb290ae
Christoph Mertins <c.mertins@gmail.com>
2018-11-12 15:14:52 +0100

    add empty tests

---
+++ src/test/java/cm/homeautomation/services/actor/ActorServiceTest.java
@@ -0,0 +1,68 @@
+package cm.homeautomation.services.actor;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class ActorServiceTest {
+
+	@BeforeEach
+	public void setup() {
+		
+	}
+
+	@Test
+	public void testCronPressSwitch() throws Exception {
+
+	}
+
+	@Test
+	public void testGetInstance() throws Exception {
+
+	}
+
+	@Test
+	public void testSetInstance() throws Exception {
+
+	}
+
+	@Test
+	public void testConnectionLost() throws Exception {
+
+	}
+
+	@Test
+	public void testDeliveryComplete() throws Exception {
+
+	}
+
+	@Test
+	public void testGetSwitchStatusesForRoom() throws Exception {
+
+	}
+
+	@Test
+	public void testGetThermostatStatusesForRoom() throws Exception {
+
+	}
+
+	@Test
+	public void testMessageArrived() throws Exception {
+
+	}
+
+	@Test
+	public void testPressSwitchStringString() throws Exception {
+
+	}
+
+	@Test
+	public void testPressSwitchStringArray() throws Exception {
+
+	}
+
+	@Test
+	public void testUpdateBackendSwitchState() throws Exception {
+
+	}
+
+}
