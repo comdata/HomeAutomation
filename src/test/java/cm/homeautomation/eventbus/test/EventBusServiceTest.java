60b9bcd26ceb5ca13f800ebb2cba271a669b98fd
Christoph Mertins <c.mertins@gmail.com>
2018-11-13 14:57:46 +0100

    add test case

--- src/test/java/cm/homeautomation/eventbus/test/EventBusServiceTest.java
+++ src/test/java/cm/homeautomation/eventbus/test/EventBusServiceTest.java
@@ -1,66 +1,72 @@
 package cm.homeautomation.eventbus.test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
 
-import org.junit.Before;
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import cm.homeautomation.eventbus.CustomEventBus;
 import cm.homeautomation.eventbus.EventBusService;
 
 public class EventBusServiceTest {
 
-	
-	@Before
+	@BeforeEach
 	public void setup() {
 		EventBusService.init();
 	}
 
-	
 	@Test
 	public void testEventBusService() throws Exception {
 		CustomEventBus eventBus = EventBusService.getEventBus();
-		
+
 		assertNotNull(eventBus);
 	}
-	
-	
+
 	@Test
 	public void testMultiCallEventBusService() throws Exception {
 		CustomEventBus eventBus = EventBusService.getEventBus();
 		CustomEventBus eventBus2 = EventBusService.getEventBus();
-		
+
 		assertEquals(eventBus, eventBus2);
 	}
-	
+
 	@Test
 	public void testAlreadyRegistered() throws Exception {
 		CustomEventBus eventBus = EventBusService.getEventBus();
-		
+
 		EventBusServiceTestSubscriber tvService = new EventBusServiceTestSubscriber();
 		eventBus.register(tvService);
-		
-		assertTrue(eventBus.getClasses().size()==1);
-		
+
+		assertTrue(eventBus.getClasses().size() == 1);
+
 		eventBus.register(tvService);
-		assertTrue(eventBus.getClasses().size()==1);
+		assertTrue(eventBus.getClasses().size() == 1);
 		eventBus.unregister(tvService);
 	}
-	
+
 	@Test
 	public void testUnregister() throws Exception {
 		CustomEventBus eventBus = EventBusService.getEventBus();
-		
+
 		EventBusServiceTestSubscriber tvService = new EventBusServiceTestSubscriber();
 		eventBus.register(tvService);
-		
-		assertTrue(eventBus.getClasses().size()==1);
-		
+
+		assertTrue(eventBus.getClasses().size() == 1);
+
 		eventBus.unregister(tvService);
-		assertTrue(eventBus.getClasses().size()==0);
+		assertTrue(eventBus.getClasses().size() == 0);
 	}
-	
-	
+
+	@Test
+	public void testInit() throws Exception {
+
+		// default test
+		EventBusService.init();
+		
+		assertNotNull(EventBusService.getEventBus());
+	}
+
 }
