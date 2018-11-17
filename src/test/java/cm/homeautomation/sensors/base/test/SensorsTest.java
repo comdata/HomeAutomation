d3f13b0edfc19006a5227c04fff2f0fbd2b9e4d4
Christoph Mertins <c.mertins@gmail.com>
2018-11-17 11:39:44 +0100

    upgrade test

--- src/test/java/cm/homeautomation/sensors/base/test/SensorsTest.java
+++ src/test/java/cm/homeautomation/sensors/base/test/SensorsTest.java
@@ -1,10 +1,13 @@
 package cm.homeautomation.sensors.base.test;
 
-import static org.junit.Assert.*;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.ArrayList;
 import java.util.List;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import cm.homeautomation.sensors.base.Sensors;
 import cm.homeautomation.sensors.base.TechnicalSensor;
@@ -20,6 +23,16 @@ public class SensorsTest {
 	public void testGetSensors() {
 		Sensors instance = Sensors.getInstance();
 
+		List<TechnicalSensor> sensors = instance.getSensors();
+		assertNotNull(sensors);
+		assertTrue(sensors.isEmpty());
+	}
+	
+	@Test
+	public void testSetSensors() {
+		Sensors instance = Sensors.getInstance();
+
+		instance.setSensors(new ArrayList<TechnicalSensor>());
 		List<TechnicalSensor> sensors = instance.getSensors();
 		assertNotNull(sensors);
 		assertTrue(sensors.isEmpty());
