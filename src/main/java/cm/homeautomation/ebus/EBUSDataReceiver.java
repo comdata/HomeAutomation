ff5cf8d6312881d5e29400002ba8069969075897
Christoph Mertins <c.mertins@gmail.com>
2018-11-20 21:55:34 +0100

    add ebus to eventbus

--- src/main/java/cm/homeautomation/ebus/EBUSDataReceiver.java
+++ src/main/java/cm/homeautomation/ebus/EBUSDataReceiver.java
@@ -2,6 +2,9 @@ package cm.homeautomation.ebus;
 
 import org.apache.logging.log4j.LogManager;
 
+import cm.homeautomation.eventbus.EventBusService;
+import cm.homeautomation.eventbus.EventObject;
+
 public class EBUSDataReceiver {
 	
 	private EBUSDataReceiver() {
@@ -10,6 +13,8 @@ public class EBUSDataReceiver {
 
 	public static void receiveEBUSData(String topic, String messageContent) {
 		LogManager.getLogger(EBUSDataReceiver.class).debug("EBUS. Topic: "+ topic+" message: "+messageContent );
+		EBusMessageEvent ebusMessageEvent=new EBusMessageEvent(topic, messageContent);
+		EventBusService.getEventBus().post(new EventObject(ebusMessageEvent));
 	}
 
 }
