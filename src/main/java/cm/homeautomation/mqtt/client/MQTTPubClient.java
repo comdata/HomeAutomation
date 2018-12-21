8e9475c5e1a1e157880ee39b3eb0c5181cd833ce
Christoph Mertins <c.mertins@gmail.com>
2018-12-21 09:10:26 +0100

    add MQTT publish client

---
+++ src/main/java/cm/homeautomation/mqtt/client/MQTTPubClient.java
@@ -0,0 +1,41 @@
+package cm.homeautomation.mqtt.client;
+
+import org.apache.logging.log4j.LogManager;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+
+public class MQTTPubClient {
+
+	
+    public static void publish(String[] args) {
+
+        String topic        = args[0];
+        String content      = "";
+        int qos             = 2;
+        String broker       = "tcp://localhost:1883";
+        String clientId     = "HA ClientId";
+        MemoryPersistence persistence = new MemoryPersistence();
+
+        try {
+            MqttClient sampleClient = new MqttClient(broker, clientId, persistence);
+            MqttConnectOptions connOpts = new MqttConnectOptions();
+            connOpts.setCleanSession(true);
+            LogManager.getLogger(MQTTPubClient.class).debug("Connecting to broker: "+broker);
+            sampleClient.connect(connOpts);
+            LogManager.getLogger(MQTTPubClient.class).debug("Connected");
+            LogManager.getLogger(MQTTPubClient.class).debug("Publishing message: "+content);
+            MqttMessage message = new MqttMessage(content.getBytes());
+            message.setQos(qos);
+            sampleClient.publish(topic, message);
+            LogManager.getLogger(MQTTPubClient.class).debug("Message published");
+            sampleClient.disconnect();
+            LogManager.getLogger(MQTTPubClient.class).debug("Disconnected");
+            System.exit(0);
+        } catch(MqttException me) {
+        	LogManager.getLogger(MQTTPubClient.class).error("MQTT Exception "+me.getReasonCode(), me);
+        }
+    }
+}
