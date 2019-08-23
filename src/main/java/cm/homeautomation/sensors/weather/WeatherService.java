23d00d259e45fc6c658f33df37a84b479512550b
Christoph Mertins <c.mertins@gmail.com>
2019-08-23 10:49:59 +0200

    change logging

--- src/main/java/cm/homeautomation/sensors/weather/WeatherService.java
+++ src/main/java/cm/homeautomation/sensors/weather/WeatherService.java
@@ -29,7 +29,7 @@ public class WeatherService {
 		Properties props = new Properties();
 		try {
 			File file = new File("weather.properties");
-			LogManager.getLogger(WeatherService.class).info("Weather properties: %s", file.getAbsolutePath());
+			LogManager.getLogger(WeatherService.class).info("Weather properties: {}", file.getAbsolutePath());
 			FileReader fileReader = new FileReader(file);
 
 			props.load(fileReader);
