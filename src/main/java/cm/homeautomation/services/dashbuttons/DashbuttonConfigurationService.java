c754d42e36ddb210ae19f5af5755ad57761d2db6
Christoph Mertins <c.mertins@gmail.com>
2019-09-16 13:13:00 +0200

    add dashbutton readAll service

---
+++ src/main/java/cm/homeautomation/services/dashbuttons/DashbuttonConfigurationService.java
@@ -0,0 +1,22 @@
+package cm.homeautomation.services.dashbuttons;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.ws.rs.Path;
+
+import cm.homeautomation.db.EntityManagerService;
+import cm.homeautomation.entities.DashButton;
+
+@Path("dashbutton")
+public class DashbuttonConfigurationService {
+
+	@Path("readAll")
+	public List<DashButton> getDashbuttons() {
+		
+		EntityManager em = EntityManagerService.getManager();
+		
+		return em.createQuery("select db from DashButton db", DashButton.class).getResultList();
+	}
+	
+}
