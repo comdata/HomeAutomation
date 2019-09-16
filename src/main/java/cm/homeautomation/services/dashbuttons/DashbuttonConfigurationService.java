715350baed6b2b55ee123de04089fedfe3d4299a
Christoph Mertins <c.mertins@gmail.com>
2019-09-16 15:43:12 +0200

    create new dashbutton

--- src/main/java/cm/homeautomation/services/dashbuttons/DashbuttonConfigurationService.java
+++ src/main/java/cm/homeautomation/services/dashbuttons/DashbuttonConfigurationService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.core.GenericEntity;
 
 import cm.homeautomation.db.EntityManagerService;
@@ -23,6 +25,30 @@ public class DashbuttonConfigurationService extends BaseService {
 		List<DashButton> resultList=em.createQuery("select db from DashButton db", DashButton.class).getResultList();
 		
 		return new GenericEntity<List<DashButton>>(resultList) {};
+	}
+	
+	@Path("update")
+	@POST
+	public void updateDashButton(DashButton dashbutton) {
+		// TODO implement
+	}
+	
+	@GET
+	@Path("new/{name}/{mac}")
+	public DashButton createNewDashButton(@PathParam("name") String name, @PathParam("mac") String mac) {
+		EntityManager em = EntityManagerService.getManager();
+		
+		em.getTransaction().begin();
+		
+		DashButton dashButton = new DashButton();
+		dashButton.setName(name);
+		dashButton.setMac(mac);
+		em.persist(dashButton);
+		em.flush();
+		em.getTransaction().commit();
+		
+		return dashButton;
+		
 	}
 	
 }
