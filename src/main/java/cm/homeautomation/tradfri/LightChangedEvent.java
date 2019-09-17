7b40ea4db71f686db053bfcdca30f743216d0816
Christoph Mertins <c.mertins@gmail.com>
2019-09-17 11:13:35 +0200

    change implementation details

---
+++ src/main/java/cm/homeautomation/tradfri/LightChangedEvent.java
@@ -0,0 +1,16 @@
+package cm.homeautomation.tradfri;
+
+import cm.homeautomation.entities.Light;
+
+public class LightChangedEvent {
+	private final Light light;
+
+	public LightChangedEvent(final Light light) {
+		this.light = light;
+
+	}
+
+	public Light getLight() {
+		return light;
+	}
+}
--- src/main/java/cm/homeautomation/tradfri/TradfriStartupService.java
+++ src/main/java/cm/homeautomation/tradfri/TradfriStartupService.java
@@ -3,14 +3,23 @@ package cm.homeautomation.tradfri;
 import java.util.Collection;
 
 import javax.crypto.SecretKey;
+import javax.persistence.EntityManager;
 
 import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.logging.log4j.LogManager;
 
 import cm.homeautomation.configuration.ConfigurationService;
+import cm.homeautomation.db.EntityManagerService;
+import cm.homeautomation.entities.DimmableLight;
+import cm.homeautomation.entities.RGBLight;
+import cm.homeautomation.eventbus.EventBusService;
+import cm.homeautomation.eventbus.EventObject;
 import cm.homeautomation.services.base.AutoCreateInstance;
+
+import cm.homeautomation.services.light.LightService;
 import de.eckey.tradfrj.item.device.Device;
 import de.eckey.tradfrj.item.device.light.Light;
+import de.eckey.tradfrj.item.device.light.LightData;
 import de.eckey.tradfrj.request.TradfrjRequestExecutor;
 import de.eckey.tradfrj.request.TradfrjRequests;
 import de.eckey.tradfrj.request.item.device.lamp.ModifyLightRequestBuilder;
@@ -22,10 +31,14 @@ import de.eckey.tradfrj.service.security.SimpleUserPskStore;
 @AutoCreateInstance
 public class TradfriStartupService {
 
+	
+	private static final String TRADFRI = "TRADFRI";
+	private EntityManager em;
+	
 	private static TradfriStartupService instance;
 	private TradfrjRequestExecutor executor;
 
-	Collection<Device> deviceList = null;
+	Collection<Light> lightList = null;
 
 	public static TradfriStartupService getInstance() {
 		return instance;
@@ -84,11 +97,54 @@ public class TradfriStartupService {
 	private void updateDevices() throws ServiceException {
 		Collection<Double> deviceIds = executor.executeRequest(TradfrjRequests.lookupDeviceIDs());
 
-		deviceList = executor.executeRequest(TradfrjRequests.lookupDevices(deviceIds));
+		lightList = executor.executeRequest(TradfrjRequests.lookupLights(deviceIds));
 
-		for (Device device : deviceList) {
-			LogManager.getLogger(this.getClass()).debug("Found {} name: {}", device.getId(), device.getName());
+		for (Light deviceLight : lightList) {
+			LogManager.getLogger(this.getClass()).debug("Found {} name: {}", deviceLight.getId(), deviceLight.getName());
+		
+			LogManager.getLogger(this.getClass()).trace("Bulb event registered");
+
+			final cm.homeautomation.entities.Light light = LightService.getInstance().getLightForTypeAndExternalId("TRADFRI",
+					Integer.toString(deviceLight.getId()));
+
+			em = EntityManagerService.getNewManager();
+			em.getTransaction().begin();
+
+			if (light instanceof DimmableLight) {
+				final DimmableLight dimLight = (DimmableLight) light;
+				final int intensity = deviceLight.getLightData()[0].getDimmer();
+
+				if (deviceLight.getReachable()==1) {
+					dimLight.setBrightnessLevel(intensity);
+				} else {
+					dimLight.setBrightnessLevel(dimLight.getMinimumValue());
+				}
+
+			}
+
+			if (light instanceof RGBLight) {
+				final RGBLight rgbLight = (RGBLight) light;
+				rgbLight.setColor( deviceLight.getLightData()[0].getColor());
+
+			}
+
+			if (deviceLight.getReachable()==1) {
+				// set on or off
+				light.setPowerState( deviceLight.getLightData()[0].getOnOff()==1);
+			} else {
+				light.setPowerState(false);
+			}
+
+			EventBusService.getEventBus().post(new EventObject(new LightChangedEvent(light)));
+
+			em.merge(light);
+
+			em.getTransaction().commit();
+			LogManager.getLogger(this.getClass()).trace("Bulb event done");
 		}
+		
+		
+		
 	}
 
 	private String createToken(TradfrjService service, String secret, String user) throws ServiceException {
