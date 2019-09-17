dcaf48144d07a5d6a78d41d31f309c351f0de3e2
Christoph Mertins <c.mertins@gmail.com>
2019-09-17 10:41:47 +0200

    remove main

--- src/main/java/cm/homeautomation/tradfri/Main.java
+++ src/main/java/cm/homeautomation/tradfri/Main.java
@@ -2,17 +2,14 @@ package cm.homeautomation.tradfri;
 
 public class Main {
 
-	protected static String gateway_ip = "192.168.1.115";
-	protected static String security_key = "BOnI8cZukkydrxBp";
-
 	public static void main(final String[] args) {
 
-		final TradfriGateway gw = new TradfriGateway(gateway_ip, security_key);
-
-		final TradfriGatewayListener gatewayListener = new TradfriGatewaylistenerImpl();
-		gw.addTradfriGatewayListener(gatewayListener);
-		gw.initCoap();
-		gw.dicoverBulbs();
+//		final TradfriGateway gw = new TradfriGateway(gateway_ip, security_key);
+//
+//		final TradfriGatewayListener gatewayListener = new TradfriGatewaylistenerImpl();
+//		gw.addTradfriGatewayListener(gatewayListener);
+//		gw.initCoap();
+//		gw.dicoverBulbs();
 
 
 
