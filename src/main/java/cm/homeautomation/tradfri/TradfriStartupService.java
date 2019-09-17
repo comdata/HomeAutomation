ba84963aeb027e96193202bbfbf7abbe7575c804
Christoph Mertins <c.mertins@gmail.com>
2019-09-17 10:34:56 +0200

    change tradfri implementation

--- src/main/java/cm/homeautomation/tradfri/TradfriStartupService.java
+++ src/main/java/cm/homeautomation/tradfri/TradfriStartupService.java
@@ -1,14 +1,31 @@
 package cm.homeautomation.tradfri;
 
+import java.util.Collection;
+
+import javax.crypto.SecretKey;
+
+import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.logging.log4j.LogManager;
 
 import cm.homeautomation.configuration.ConfigurationService;
 import cm.homeautomation.services.base.AutoCreateInstance;
+import de.eckey.tradfrj.item.device.Device;
+import de.eckey.tradfrj.item.device.light.Light;
+import de.eckey.tradfrj.request.TradfrjRequestExecutor;
+import de.eckey.tradfrj.request.TradfrjRequests;
+import de.eckey.tradfrj.request.item.device.lamp.ModifyLightRequestBuilder;
+import de.eckey.tradfrj.service.ServiceException;
+import de.eckey.tradfrj.service.TradfrjService;
+import de.eckey.tradfrj.service.lookup.AuthoritySupplier;
+import de.eckey.tradfrj.service.security.SimpleUserPskStore;
 
 @AutoCreateInstance
 public class TradfriStartupService {
 
 	private static TradfriStartupService instance;
+	private TradfrjRequestExecutor executor;
+
+	Collection<Device> deviceList = null;
 
 	public static TradfriStartupService getInstance() {
 		return instance;
@@ -18,8 +35,6 @@ public class TradfriStartupService {
 		TradfriStartupService.instance = instance;
 	}
 
-	private TradfriGateway gw;
-
 	public TradfriStartupService() {
 		instance = this;
 		init();
@@ -27,51 +42,83 @@ public class TradfriStartupService {
 	}
 
 	public void dimBulb(final String id, final int dimValue) {
+		
+		Light light = new Light();
+		light.setId(Integer.parseInt(id));
+		ModifyLightRequestBuilder builder = ModifyLightRequestBuilder.modify(light);
+		builder.withDimmer(dimValue);
+		LogManager.getLogger(this.getClass()).debug("setting dimValue: {}", dimValue);
 
-		for (final LightBulb b : gw.bulbs) {
-			if (Integer.toString(b.getId()).equals(id)) {
-				b.setIntensity(dimValue);
-			}
-
+		try {
+			executor.executeRequest(builder);
+		} catch (ServiceException e) {
+			LogManager.getLogger(this.getClass()).debug("setting dim value failed", e);
 		}
 
 	}
 
 	private void init() {
 
-		gw = new TradfriGateway(ConfigurationService.getConfigurationProperty("tradfri", "gateway"),
-				ConfigurationService.getConfigurationProperty("tradfri", "secret"));
-		final TradfriGatewayListener gatewayListener = new TradfriGatewaylistenerImpl();
-		gw.addTradfriGatewayListener(gatewayListener);
+		String gateway = ConfigurationService.getConfigurationProperty("tradfri", "gateway");
+		String secret = ConfigurationService.getConfigurationProperty("tradfri", "secret");
+		String token = ConfigurationService.getConfigurationProperty("tradfri", "token");
+		String user = ConfigurationService.getConfigurationProperty("tradfri", "user");
 
-		gw.initCoap();
-		gw.dicoverBulbs();
+		AuthoritySupplier authoritySupplier = new AuthoritySupplier(gateway, 5684);
+		TradfrjService service = new TradfrjService(authoritySupplier);
 
-		final Runnable tradfriRunner = new Runnable() {
-			@Override
-			public void run() {
-				try {
-					gw.startTradfriGateway();
-				} catch (final Exception e) {
-					LogManager.getLogger(this.getClass()).info("Failed creating class");
-				}
-			}
-		};
-		new Thread(tradfriRunner).start();
+		try {
+			createPSK(service, secret, user, token);
 
+			service.start();
+			executor = new TradfrjRequestExecutor(service);
+
+			updateDevices();
+
+		} catch (ServiceException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	private void updateDevices() throws ServiceException {
+		Collection<Double> deviceIds = executor.executeRequest(TradfrjRequests.lookupDeviceIDs());
+
+		deviceList = executor.executeRequest(TradfrjRequests.lookupDevices(deviceIds));
+
+		for (Device device : deviceList) {
+			LogManager.getLogger(this.getClass()).debug("Found {} name: {}", device.getId(), device.getName());
+		}
+	}
+
+	private String createToken(TradfrjService service, String secret, String user) throws ServiceException {
+		return service.authenticateUser(secret, user);
+	}
+
+	private void createPSK(TradfrjService service, String secret, String user, String token) throws ServiceException {
+
+		if (token.isEmpty()) {
+			user = RandomStringUtils.randomAlphanumeric(8);
+			token = createToken(service, secret, user);
+			ConfigurationService.createOrUpdate("tradfri", "token", token);
+			ConfigurationService.createOrUpdate("tradfri", "user", user);
+		}
+
+		service.setPskStore(new SimpleUserPskStore(user, token));
 	}
 
 	public void setColor(final String id, final String color) {
 
-		for (final LightBulb b : gw.bulbs) {
-			if (Integer.toString(b.getId()).equals(id)) {
-				b.setColor(color.toLowerCase());
+		Light light = new Light();
+		light.setId(Integer.parseInt(id));
+		ModifyLightRequestBuilder builder = ModifyLightRequestBuilder.modify(light);
+		builder.withColor(color.toLowerCase());
+		LogManager.getLogger(this.getClass()).debug("setting color: {}", color);
 
-				LogManager.getLogger(this.getClass()).error("setting color: " + b.getColor());
-				return;
-			}
-
+		try {
+			executor.executeRequest(builder);
+		} catch (ServiceException e) {
+			LogManager.getLogger(this.getClass()).debug("setting color failed", e);
 		}
-
 	}
 }
