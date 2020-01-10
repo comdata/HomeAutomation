80924b384b3347afc2305f6850bce17e7402fadb
Christoph Mertins <c.mertins@gmail.com>
2020-01-10 21:37:18 +0100

    register eventbus

--- src/main/java/cm/homeautomation/mqtt/topicrecorder/MQTTTopicRecorder.java
+++ src/main/java/cm/homeautomation/mqtt/topicrecorder/MQTTTopicRecorder.java
@@ -8,6 +8,8 @@ import org.greenrobot.eventbus.Subscribe;
 
 import cm.homeautomation.db.EntityManagerService;
 import cm.homeautomation.entities.MQTTTopic;
+import cm.homeautomation.eventbus.EventBusService;
+import cm.homeautomation.services.base.AutoCreateInstance;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import lombok.extern.log4j.Log4j2;
@@ -19,30 +21,33 @@ import lombok.extern.log4j.Log4j2;
  *
  */
 @Log4j2
-@NoArgsConstructor
+@AutoCreateInstance
 public class MQTTTopicRecorder {
 
-	
-	
+	public MQTTTopicRecorder() {
+		EventBusService.getEventBus().register(this);
+	}
+
 	@Subscribe
 	public void receiverMQTTTopicEvents(MQTTTopicEvent event) {
 		EntityManager em = EntityManagerService.getNewManager();
-		
+
 		@NonNull
 		String topic = event.getTopic();
 		log.debug(topic);
-		
-		List<MQTTTopic> mqttTopicList = em.createQuery("select t from MQTTTopic t where topic=:topic", MQTTTopic.class).setParameter("topic", topic).getResultList();
-		
-		if (mqttTopicList==null || mqttTopicList.isEmpty()) {
+
+		List<MQTTTopic> mqttTopicList = em.createQuery("select t from MQTTTopic t where topic=:topic", MQTTTopic.class)
+				.setParameter("topic", topic).getResultList();
+
+		if (mqttTopicList == null || mqttTopicList.isEmpty()) {
 			em.getTransaction().begin();
 			MQTTTopic mqttTopic = new MQTTTopic(topic);
-			
+
 			em.persist(mqttTopic);
 			em.getTransaction().commit();
-			log.debug("MQTT topic created. Topic: "+topic);
+			log.debug("MQTT topic created. Topic: " + topic);
 		}
-		
+
 	}
-	
+
 }
