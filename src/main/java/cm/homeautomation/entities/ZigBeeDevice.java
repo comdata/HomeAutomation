48a20bf66500d1dd4b889602fab256b39bdb2bc7
Christoph Mertins <c.mertins@gmail.com>
2020-04-19 12:49:53 +0200

    add device message handling

---
+++ src/main/java/cm/homeautomation/entities/ZigBeeDevice.java
@@ -0,0 +1,32 @@
+package cm.homeautomation.entities;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class ZigBeeDevice {
+
+	// {"ieeeAddr":"0x00124b0018e27f02","type":"Coordinator","networkAddress":0,
+	// "friendly_name":"Coordinator","softwareBuildID":"zStack12","dateCode":"20190608","lastSeen":1587283920055}}
+
+	@Id
+	String ieeeAddr;
+	String type;
+	int networkAddress;
+
+	@JsonAlias("friendly_name")
+	String friendlyName;
+	String softwareBuildID;
+	String dateCode;
+	Date lastSeen;
+
+}
--- src/main/java/cm/homeautomation/zigbee/ZigbeeMQTTReceiver.java
+++ src/main/java/cm/homeautomation/zigbee/ZigbeeMQTTReceiver.java
@@ -8,10 +8,17 @@ import javax.persistence.EntityManager;
 import org.apache.logging.log4j.LogManager;
 import org.greenrobot.eventbus.Subscribe;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import cm.homeautomation.configuration.ConfigurationService;
 import cm.homeautomation.db.EntityManagerService;
 import cm.homeautomation.entities.MQTTTopic;
+import cm.homeautomation.entities.ZigBeeDevice;
 import cm.homeautomation.eventbus.EventBusService;
+import cm.homeautomation.mqtt.client.MQTTSender;
 import cm.homeautomation.mqtt.topicrecorder.MQTTTopicEvent;
 import cm.homeautomation.mqtt.topicrecorder.MQTTTopicRecorder;
 import cm.homeautomation.services.base.AutoCreateInstance;
@@ -22,13 +29,19 @@ import lombok.extern.log4j.Log4j2;
 @AutoCreateInstance
 public class ZigbeeMQTTReceiver {
 
-    private static List<String> topicList=new ArrayList<>();
+	private static List<String> topicList = new ArrayList<>();
 	private String zigbeeMqttTopic;
 
 	public ZigbeeMQTTReceiver() {
 		EventBusService.getEventBus().register(this);
-		
+
 		zigbeeMqttTopic = ConfigurationService.getConfigurationProperty("zigbee", "mqttTopic");
+
+		updateDeviceList();
+	}
+
+	private void updateDeviceList() {
+		MQTTSender.sendMQTTMessage(zigbeeMqttTopic + "/bridge/config/devices/get", "");
 	}
 
 	@Subscribe
@@ -40,8 +53,39 @@ public class ZigbeeMQTTReceiver {
 		log.debug(topic);
 
 		if (topic.startsWith(zigbeeMqttTopic)) {
+
 			// do zigbee magic
-			LogManager.getLogger(this.getClass()).error("Got Zigbee message: "+event.getMessage());
+			String message = event.getMessage();
+			LogManager.getLogger(this.getClass()).error("Got Zigbee message: " + message);
+
+			if (topic.equals(zigbeeMqttTopic + "/bridge/config/devices")) {
+				handleDeviceMessage(message);
+
+			}
+		}
+	}
+
+	private void handleDeviceMessage(String message) {
+		try {
+			ObjectMapper mapper = new ObjectMapper();
+
+			List<ZigBeeDevice> deviceList = mapper.readValue(message, new TypeReference<List<ZigBeeDevice>>() {
+			});
+
+			EntityManager em = EntityManagerService.getManager();
+
+			for (ZigBeeDevice zigBeeDevice : deviceList) {
+				try {
+					em.getTransaction().begin();
+					em.persist(zigBeeDevice);
+					em.getTransaction().commit();
+				} catch (Exception e) {
+					
+				}
+			}
+
+		} catch (JsonProcessingException e) {
+			LogManager.getLogger(this.getClass()).error("error parsing zigbee device message " + message, e);
 		}
 	}
 
