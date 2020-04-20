abd550b5d8e6006202f7e58e70599fb04c3ea38a
Christoph Mertins <c.mertins@gmail.com>
2020-04-20 09:30:17 +0200

    handle no subscribers for an event

---
+++ src/main/java/cm/homeautomation/eventbus/NoSubscriberEventListener.java
@@ -0,0 +1,22 @@
+package cm.homeautomation.eventbus;
+
+import org.apache.logging.log4j.LogManager;
+import org.greenrobot.eventbus.NoSubscriberEvent;
+import org.greenrobot.eventbus.Subscribe;
+
+import cm.homeautomation.services.base.AutoCreateInstance;
+
+@AutoCreateInstance
+public class NoSubscriberEventListener {
+
+	public NoSubscriberEventListener() {
+		EventBusService.getEventBus().register(this);
+	}
+
+	@Subscribe
+	public void subscribe(NoSubscriberEvent event) {
+
+		LogManager.getLogger(this.getClass())
+				.error("no subscriber for event: " + event.originalEvent.getClass().getSimpleName());
+	}
+}
