928d7df71329e3f1f37e7be0e291a202e72814a1
Christoph Mertins <c.mertins@gmail.com>
2020-04-22 22:50:36 +0200

    check for null

--- src/main/java/cm/homeautomation/services/hueinterface/HueInterface.java
+++ src/main/java/cm/homeautomation/services/hueinterface/HueInterface.java
@@ -24,9 +24,10 @@ public class HueInterface extends BaseService {
 	public GenericStatus handleMessage(HueEmulatorMessage message) {
 		EntityManager em = EntityManagerService.getManager();
 
+		String lightId = message.getLightId();
 		List<HueDevice> hueDeviceList = em.createQuery("select hd from HueDevice hd where hd.lightId=:lightId",
 				HueDevice.class)
-				.setParameter("lightId", message.getLightId()).getResultList();
+				.setParameter("lightId", lightId).getResultList();
 
 		if (hueDeviceList == null || hueDeviceList.isEmpty()) {
 			em.getTransaction().begin();
@@ -39,36 +40,39 @@ public class HueInterface extends BaseService {
 		} else {
 			HueDevice hueDevice = hueDeviceList.get(0);
 
-			switch (hueDevice.getType()) {
-			case LIGHT:
-				LightService.getInstance().setLightState(hueDevice
-						.getExternalId(),
-						("on".equals(message.getPayload()) ? LightStates.ON : LightStates.OFF));
+			if (hueDevice != null) {
 
-				if (!message.isOnOffCommand()) {
-					LightService.getInstance().dimLight(hueDevice.getExternalId(), message.getBrightness());
+				switch (hueDevice.getType()) {
+				case LIGHT:
+					LightService.getInstance().setLightState(hueDevice.getExternalId(),
+							("on".equals(message.getPayload()) ? LightStates.ON : LightStates.OFF));
+
+					if (!message.isOnOffCommand()) {
+						LightService.getInstance().dimLight(hueDevice.getExternalId(), message.getBrightness());
+					}
+
+					break;
+				case SWITCH:
+					ActorService.getInstance().pressSwitch(Long.toString(hueDevice.getExternalId()),
+							("on".equals(message.getPayload()) ? "ON" : "OFF"));
+					break;
+				case WINDOWBLIND:
+					String dimValue = "99";
+					if (!message.isOnOffCommand()) {
+						dimValue = Integer.toString(message.getBrightness());
+					}
+					System.out.println("Window Blind dim: " + dimValue);
+
+					new WindowBlindService().setDim(hueDevice.getExternalId(),
+							("on".equals(message.getPayload()) ? dimValue : "0"),
+							hueDevice.isGroupDevice() ? WindowBlind.ALL_AT_ONCE : WindowBlind.SINGLE,
+							(hueDevice.isGroupDevice() ? hueDevice.getRoom().getId() : null));
+					break;
+				default:
+					break;
 				}
-
-				break;
-			case SWITCH:
-				ActorService.getInstance().pressSwitch(Long.toString(hueDevice
-						.getExternalId()),
-						("on".equals(message.getPayload()) ? "ON" : "OFF"));
-				break;
-			case WINDOWBLIND:
-				String dimValue = "99";
-				if (!message.isOnOffCommand()) {
-					dimValue = Integer.toString(message.getBrightness());
-				}
-				System.out.println("Window Blind dim: " + dimValue);
-
-				new WindowBlindService().setDim(hueDevice.getExternalId(),
-						("on".equals(message.getPayload()) ? dimValue : "0"),
-						hueDevice.isGroupDevice() ? WindowBlind.ALL_AT_ONCE : WindowBlind.SINGLE,
-						(hueDevice.isGroupDevice() ? hueDevice.getRoom().getId() : null));
-				break;
-			default:
-				break;
+			} else {
+				System.out.println("hue not found for lightId: " + lightId);
 			}
 		}
 
