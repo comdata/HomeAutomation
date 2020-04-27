b677ff4fa9ff1c0ed56a4309cbcd13873df50447
Christoph Mertins <c.mertins@gmail.com>
2020-04-27 16:09:50 +0200

    add color class

---
+++ src/main/java/cm/homeautomation/services/hueinterface/HueColor.java
@@ -0,0 +1,15 @@
+package cm.homeautomation.services.hueinterface;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class HueColor {
+	float x;
+	float y;
+}
