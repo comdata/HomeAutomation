f948bec7b0854aeef5c4359a81e83b0664dde695
Christoph Mertins <c.mertins@gmail.com>
2020-05-02 20:38:39 +0200

    logging

--- src/main/java/cm/homeautomation/entities/LoggingEntity.java
+++ src/main/java/cm/homeautomation/entities/LoggingEntity.java
@@ -11,16 +11,12 @@ import org.apache.logging.log4j.core.appender.db.jpa.BasicLogEventEntity;
 import org.eclipse.persistence.annotations.Index;
 
 import lombok.AllArgsConstructor;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Entity
 @Table(name="LOGGING")
 @Index(name = "ix_timemillis", columnNames = { "timeMillis" })
 @Index(name = "ix_source", columnNames = { "source", "timeMillis" })
-@Getter
-@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class LoggingEntity extends BasicLogEventEntity {
@@ -30,12 +26,20 @@ public class LoggingEntity extends BasicLogEventEntity {
 	 */
     private static final long serialVersionUID = 1L;
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id = 0L;
 	
 	public LoggingEntity(final LogEvent wrappedEvent) {
 		super(wrappedEvent);
+	}
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
 	}
 
 	
--- src/main/java/cm/homeautomation/logging/LogDemo.java
+++ src/main/java/cm/homeautomation/logging/LogDemo.java
@@ -3,6 +3,8 @@ package cm.homeautomation.logging;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import cm.homeautomation.configuration.ConfigurationService;
+
 /**
  * Created by sadupa on 7/26/14.
  */
@@ -10,7 +12,12 @@ public class LogDemo {
 
 	private static final Logger logger = LogManager.getLogger(LogDemo.class.getName());
 
+
 	public static void main(String[] args) {
+		ConfigurationService.createOrUpdate("logging", "log4j", "src/main/java/log4j.xml");
+		
+		new LogConfigurator();
+
 		logger.debug("Hello world - debug log");
 		logger.info("Hello world - info log");
 		logger.warn("Hello world - warn log");
