bbb0225d7d51b5a7d308d8e66ba9425f9717e337
Christoph Mertins <c.mertins@gmail.com>
2020-07-10 13:18:46 +0200

    change to motionevent

--- src/main/java/cm/homeautomation/services/motion/MotionDetectionService.java
+++ src/main/java/cm/homeautomation/services/motion/MotionDetectionService.java
@@ -3,7 +3,6 @@ package cm.homeautomation.services.motion;
 import org.greenrobot.eventbus.Subscribe;
 
 import cm.homeautomation.eventbus.EventBusService;
-import cm.homeautomation.eventbus.EventObject;
 import cm.homeautomation.services.base.BaseService;
 
 public class MotionDetectionService extends BaseService {
@@ -14,19 +13,14 @@ public class MotionDetectionService extends BaseService {
 	}
 
 	@Subscribe
-	public void registerMotionEvent(final EventObject event) {
+	public void registerMotionEvent(final MotionEvent motionEvent) {
 
-		final Object eventData = event.getData();
-		if (eventData instanceof MotionEvent) {
-			final MotionEvent motionEvent = (MotionEvent) eventData;
+		final boolean state = motionEvent.isState();
 
-			final boolean state = motionEvent.isState();
+		// TODO refactor this. add suppression and flip detection logic
+		// add
+		System.out.println(motionEvent.getMessageString());
 
-			// TODO refactor this. add suppression and flip detection logic
-			// add
-			System.out.println(motionEvent.getMessageString());
-
-		}
 	}
 
 }
