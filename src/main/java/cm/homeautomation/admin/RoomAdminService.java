9a3550ccc7cbcfec2fa753efadcdbd6e0808b7ce
Christoph Mertins <c.mertins@gmail.com>
2020-08-03 09:32:04 +0000

    remove import

--- src/main/java/cm/homeautomation/admin/RoomAdminService.java
+++ src/main/java/cm/homeautomation/admin/RoomAdminService.java
@@ -8,7 +8,6 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 
-import cm.homeautomation.db.EntityManagerService;
 import cm.homeautomation.entities.Room;
 import cm.homeautomation.services.base.BaseService;
 
