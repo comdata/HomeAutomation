1b3359e8d0238a1b3af7237f679e851a1deeac5c
Christoph Mertins <c.mertins@gmail.com>
2020-08-03 09:39:36 +0000

    add lombok getter/setter

--- src/main/java/cm/homeautomation/entities/Device.java
+++ src/main/java/cm/homeautomation/entities/Device.java
@@ -12,9 +12,13 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import com.fasterxml.jackson.annotation.JsonBackReference;
 
-
+@Getter
+@Setter
 @Entity
 public class Device {
 
@@ -46,95 +50,6 @@ public class Device {
 	private String name;
 
 	@OneToMany
-	
+    private Map<String, Sensor> sensors;
 
-	private Map<String, Sensor> sensors;
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public Room getRoom() {
-		return room;
-	}
-
-	public void setRoom(Room room) {
-		this.room = room;
-	}
-
-	public String getMac() {
-		return mac;
-	}
-
-	public void setMac(String mac) {
-		this.mac = mac;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public Map<String, Sensor> getSensors() {
-		return sensors;
-	}
-
-	public void setSensors(Map<String, Sensor> sensors) {
-		this.sensors = sensors;
-	}
-
-	public Date getDateInstalled() {
-		return dateInstalled;
-	}
-
-	public void setDateInstalled(Date dateInstalled) {
-		this.dateInstalled = dateInstalled;
-	}
-
-	public Date getDateLastSeen() {
-		return dateLastSeen;
-	}
-
-	public void setDateLastSeen(Date dateLastSeen) {
-		this.dateLastSeen = dateLastSeen;
-	}
-
-	public String getManufacturer() {
-		return manufacturer;
-	}
-
-	public void setManufacturer(String manufacturer) {
-		this.manufacturer = manufacturer;
-	}
-
-	public String getType() {
-		return type;
-	}
-
-	public void setType(String type) {
-		this.type = type;
-	}
-
-	public String getFirmware() {
-		return firmware;
-	}
-
-	public void setFirmware(String firmware) {
-		this.firmware = firmware;
-	}
-
-	public String getExternalId() {
-		return externalId;
-	}
-
-	public void setExternalId(String externalId) {
-		this.externalId = externalId;
-	}
 }
