76c12a2690894a5e3702342e44860406a46b641f
Christoph Mertins <c.mertins@gmail.com>
2020-08-04 12:42:21 +0000

    add and fix test

--- src/test/java/cm/homeautomation/services/mqtt/test/MQTTTopicServiceTest.java
+++ src/test/java/cm/homeautomation/services/mqtt/test/MQTTTopicServiceTest.java
@@ -1,19 +1,23 @@
-package cm.homeautomation.entities.test;
-
-import de.a9d3.testing.checks.*;
-import de.a9d3.testing.executer.SingleThreadExecutor;
-import java.util.Arrays;
-import org.junit.jupiter.api.Test;
+package cm.homeautomation.services.mqtt.test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import cm.homeautomation.entities.MQTTTopic;
 
-public class MQTTTopicTest {
+import java.util.Arrays;
+
+import org.junit.jupiter.api.Test;
+
+import cm.homeautomation.services.mqtt.MQTTTopicService;
+import de.a9d3.testing.checks.GetterIsSetterCheck;
+import de.a9d3.testing.checks.PublicVariableCheck;
+import de.a9d3.testing.executer.SingleThreadExecutor;
+
+public class MQTTTopicServiceTest {
+    
     @Test
     public void baseTest() {
         SingleThreadExecutor executor = new SingleThreadExecutor();
 
-        assertTrue(executor.execute(MQTTTopic.class, Arrays.asList( 
+        assertTrue(executor.execute(MQTTTopicService.class, Arrays.asList( 
                 //new CopyConstructorCheck(), 
                 //new DefensiveCopyingCheck(),
                 //new EmptyCollectionCheck(), 
