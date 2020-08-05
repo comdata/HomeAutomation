82f556445bd3870f897070007a4ccf640092415a
Christoph Mertins <c.mertins@gmail.com>
2020-08-05 08:52:33 +0000

    use try with resources

--- src/main/java/cm/homeautomation/tv/panasonic/PanasonicTVBinding.java
+++ src/main/java/cm/homeautomation/tv/panasonic/PanasonicTVBinding.java
@@ -28,79 +28,80 @@ import org.apache.logging.log4j.LogManager;
  */
 public class PanasonicTVBinding {
 
-	/**
-	 * Listening port of the TV
-	 */
-	private static final int TV_PORT = 55000;
+    /**
+     * Listening port of the TV
+     */
+    private static final int TV_PORT = 55000;
 
-	public PanasonicTVBinding() {
-		// nothing to do here
-	}
+    public PanasonicTVBinding() {
+        // nothing to do here
+    }
 
-	public boolean checkAlive(String tvIp) {
-		boolean alive = false;
+    public boolean checkAlive(String tvIp) {
+        boolean alive = false;
 
-		int sendCommandStatus;
-		try {
-			sendCommandStatus = sendCommand(tvIp, "DUMMY");
+        int sendCommandStatus;
+        try {
+            sendCommandStatus = sendCommand(tvIp, "DUMMY");
 
-			if (sendCommandStatus == 200) {
-				alive = true;
-			}
+            if (sendCommandStatus == 200) {
+                alive = true;
+            }
 
-		} catch (TVNotReachableException e) {
-			LogManager.getLogger(this.getClass()).debug("TVNotReachableException",e );
-		}
+        } catch (TVNotReachableException e) {
+            LogManager.getLogger(this.getClass()).debug("TVNotReachableException", e);
+        }
 
-		return alive;
-	}
+        return alive;
+    }
 
-	/**
-	 * This methods sends the command to the TV
-	 * 
-	 * @return HTTP response code from the TV (should be 200)
-	 * @throws TVNotReachableException
-	 */
-	public int sendCommand(String tvIp, String command) throws TVNotReachableException {
-		final String soaprequest_skeleton = "<s:Envelope xmlns:s=\"http://schemas.xmlsoap.org/soap/envelope/\">"
-				+ "<s:Body><u:X_SendKey xmlns:u=\"urn:panasonic-com:service:p00NetworkControl:1\">"
-				+ "<X_KeyEvent>NRC_%s</X_KeyEvent></u:X_SendKey></s:Body></s:Envelope>\r";
-		String soaprequest = "";
+    /**
+     * This methods sends the command to the TV
+     * 
+     * @return HTTP response code from the TV (should be 200)
+     * @throws TVNotReachableException
+     */
+    public int sendCommand(String tvIp, String command) throws TVNotReachableException {
+        final String soaprequest_skeleton = "<s:Envelope xmlns:s=\"http://schemas.xmlsoap.org/soap/envelope/\">"
+                + "<s:Body><u:X_SendKey xmlns:u=\"urn:panasonic-com:service:p00NetworkControl:1\">"
+                + "<X_KeyEvent>NRC_%s</X_KeyEvent></u:X_SendKey></s:Body></s:Envelope>\r";
+        String soaprequest = "";
 
-		if (command.toUpperCase().startsWith("HDMI")) {
-			soaprequest = String.format(soaprequest_skeleton, command);
-		} else {
-			soaprequest = String.format(soaprequest_skeleton, command + "-ONOFF");
-		}
+        if (command.toUpperCase().startsWith("HDMI")) {
+            soaprequest = String.format(soaprequest_skeleton, command);
+        } else {
+            soaprequest = String.format(soaprequest_skeleton, command + "-ONOFF");
+        }
 
-		if ((tvIp == null) || tvIp.isEmpty()) {
-			return 0;
-		}
-		
-		try (Socket client = new Socket(tvIp, TV_PORT);
-				BufferedWriter wr = new BufferedWriter(new OutputStreamWriter(client.getOutputStream(), StandardCharsets.UTF_8));) {
+        if ((tvIp == null) || tvIp.isEmpty()) {
+            return 0;
+        }
 
-			String header = "POST /nrc/control_0/ HTTP/1.1\r\n";
-			header = header + "Host: " + tvIp + ":" + TV_PORT + "\r\n";
-			header = header + "SOAPACTION: \"urn:panasonic-com:service:p00NetworkControl:1#X_SendKey\"\r\n";
-			header = header + "Content-Type: text/xml; charset=\"utf-8\"\r\n";
-			header = header + "Content-Length: " + soaprequest.length() + "\r\n";
-			header = header + "\r\n";
+        try (Socket client = new Socket(tvIp, TV_PORT);
+                BufferedWriter wr = new BufferedWriter(
+                        new OutputStreamWriter(client.getOutputStream(), StandardCharsets.UTF_8));) {
 
-			wr.write(header);
-			wr.write(soaprequest);
+            String header = "POST /nrc/control_0/ HTTP/1.1\r\n";
+            header = header + "Host: " + tvIp + ":" + TV_PORT + "\r\n";
+            header = header + "SOAPACTION: \"urn:panasonic-com:service:p00NetworkControl:1#X_SendKey\"\r\n";
+            header = header + "Content-Type: text/xml; charset=\"utf-8\"\r\n";
+            header = header + "Content-Length: " + soaprequest.length() + "\r\n";
+            header = header + "\r\n";
 
-			wr.flush();
+            wr.write(header);
+            wr.write(soaprequest);
 
-			InputStream inFromServer = client.getInputStream();
+            wr.flush();
 
-			BufferedReader reader = new BufferedReader(new InputStreamReader(inFromServer));
+            InputStream inFromServer = client.getInputStream();
 
-			String response = reader.readLine();
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(inFromServer))) {
+                String response = reader.readLine();
 
-			return Integer.parseInt(response.split(" ")[1]);
-		} catch (Exception e) {
-			throw new TVNotReachableException();
-		}
-	}
+                return Integer.parseInt(response.split(" ")[1]);
+            }
+        } catch (Exception e) {
+            throw new TVNotReachableException();
+        }
+    }
 }
