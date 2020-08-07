36e1b0f3bceb3da94bfd78a92e37149142b009f4
Christoph Mertins <c.mertins@gmail.com>
2020-08-07 13:05:57 +0200

    add test

---
+++ src/test/java/cm/homeautomation/entities/test/RGBLightTest.java
@@ -0,0 +1,29 @@
+package cm.homeautomation.entities.test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Arrays;
+
+import org.junit.jupiter.api.Test;
+
+import cm.homeautomation.entities.RGBLight;
+import de.a9d3.testing.checks.GetterIsSetterCheck;
+import de.a9d3.testing.checks.PublicVariableCheck;
+import de.a9d3.testing.executer.SingleThreadExecutor;
+
+class RGBLightTest {
+
+	@Test
+	public void baseTest() {
+		SingleThreadExecutor executor = new SingleThreadExecutor();
+
+		assertTrue(executor.execute(RGBLight.class, Arrays.asList(
+				// new CopyConstructorCheck(),
+				// new DefensiveCopyingCheck(),
+				// new EmptyCollectionCheck(),
+				new GetterIsSetterCheck(),
+				// new HashcodeAndEqualsCheck(),
+				new PublicVariableCheck(true))));
+	}
+
+}
