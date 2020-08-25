c4429802abef6126a8e9098eb494f7ac9a14a140
Christoph Mertins <c.mertins@gmail.com>
2020-08-25 09:46:26 +0000

    cleanup

--- src/main/java/cm/homeautomation/services/networkmonitor/NetworkWakeupEvent.java
+++ src/main/java/cm/homeautomation/services/networkmonitor/NetworkWakeupEvent.java
@@ -1,19 +1,21 @@
 package cm.homeautomation.services.networkmonitor;
 
 import cm.homeautomation.messages.base.HumanMessageGenerationInterface;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@RequiredArgsConstructor
 public class NetworkWakeupEvent implements HumanMessageGenerationInterface{
 
+    @NonNull
 	private String mac;
 
-	private NetworkWakeupEvent() {
-		// do nothing
-	}
-	
-	public NetworkWakeupEvent(String mac) {
-		this.mac = mac;
-		
-	}
 	@Override
 	public String getTitle() {
 
@@ -24,14 +26,6 @@ public class NetworkWakeupEvent implements HumanMessageGenerationInterface{
 	public String getMessageString() {
 		
 		return "Device with MAC: "+getMac()+" woken up.";
-	}
-
-	public String getMac() {
-		return mac;
-	}
-
-	public void setMac(String mac) {
-		this.mac = mac;
 	}
 
 }
