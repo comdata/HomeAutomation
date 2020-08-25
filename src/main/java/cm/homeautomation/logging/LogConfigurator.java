e41614dadd1088dd6ad64bf36350014f29b06aab
Christoph Mertins <c.mertins@gmail.com>
2020-08-25 07:10:12 +0000

    cleanup

--- src/main/java/cm/homeautomation/logging/LogConfigurator.java
+++ src/main/java/cm/homeautomation/logging/LogConfigurator.java
@@ -8,7 +8,6 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.core.LoggerContext;
 
 import cm.homeautomation.configuration.ConfigurationService;
-import cm.homeautomation.services.base.AutoCreateInstance;
 
 /**
  * configure log4j to use a configured file
