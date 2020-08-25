5a979a0202f8c8adc4680ab3e8bf4253a07706a0
Christoph Mertins <c.mertins@gmail.com>
2020-08-25 07:20:51 +0000

    cleanup

--- src/main/java/cm/homeautomation/services/cameras/CameraServiceTester.java
+++ src/main/java/cm/homeautomation/services/cameras/CameraServiceTester.java
@@ -3,7 +3,6 @@ package cm.homeautomation.services.cameras;
 public class CameraServiceTester {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		String[] myargs={"1", "200", "112"};
 		CameraService.prepareCameraImage(myargs);
 	}
--- src/main/java/cm/homeautomation/services/hueinterface/HueEmulatorMessage.java
+++ src/main/java/cm/homeautomation/services/hueinterface/HueEmulatorMessage.java
@@ -26,8 +26,6 @@ public class HueEmulatorMessage {
 
 	boolean on;
 
-	// TODO xy als Array
-
 	String payload;
 
 	@JsonAlias("bri")
