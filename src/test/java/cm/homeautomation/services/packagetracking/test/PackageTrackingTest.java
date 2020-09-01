eb5c8e5b243c2c1016c14e2db72bf01e204ae26e
Christoph Mertins <c.mertins@gmail.com>
2020-09-01 07:53:16 +0000

    update test

--- src/test/java/cm/homeautomation/services/packagetracking/test/PackageTrackingTest.java
+++ src/test/java/cm/homeautomation/services/packagetracking/test/PackageTrackingTest.java
@@ -1,25 +1,54 @@
 package cm.homeautomation.services.packagetracking.test;
 
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.is;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
 import org.junit.jupiter.api.Test;
 
 import cm.homeautomation.db.EntityManagerService;
-import cm.homeautomation.entities.Room;
-import cm.homeautomation.entities.WindowBlind;
+import cm.homeautomation.entities.Package;
+import cm.homeautomation.entities.PackageHistory;
+import cm.homeautomation.entities.PackagePK;
 import io.quarkus.test.junit.QuarkusTest;
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.containsString;
-
-import javax.persistence.EntityManager;
 
 @QuarkusTest
 public class PackageTrackingTest {
     @Test
     public void testPackagesGetAllOpen() {
+        given().when().get("/packages/getAllOpen").then().statusCode(200).body(is("[]"));
+    }
+
+    @Test
+    public void testPackagesGetAllOpenWithEntity() {
+        EntityManager em = EntityManagerService.getManager();
+
+        em.getTransaction().begin();
+
+        Package pack = new Package();
+        pack.setPackageName("Test Package");
+        pack.setCarrierName("dp");
+        pack.setId(new PackagePK("test", "dp"));
+        List<PackageHistory> packageHistory=new ArrayList<>();
+        pack.setPackageHistory(packageHistory);
+        em.persist(pack);
+        em.getTransaction().commit();
+
         given()
           .when().get("/packages/getAllOpen")
           .then()
              .statusCode(200)
-             .body(is("[]"));
+             .body(containsString("\""+pack.getPackageName()+"\""));
+
+        em.getTransaction().begin();
+
+        em.createQuery("delete from Package p where p.packageName=:name").setParameter("name", pack.getPackageName()).executeUpdate();
+
+        em.getTransaction().commit();
     }
 }
