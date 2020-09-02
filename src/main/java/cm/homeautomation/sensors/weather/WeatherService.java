d53fe73e53991c521b7bcb310788b64312e62e4c
Christoph Mertins <c.mertins@gmail.com>
2020-09-02 12:58:53 +0000

    try with resources

--- src/main/java/cm/homeautomation/sensors/weather/WeatherService.java
+++ src/main/java/cm/homeautomation/sensors/weather/WeatherService.java
@@ -21,54 +21,54 @@ import cm.homeautomation.services.overview.WeatherData;
  */
 public class WeatherService {
 
-	public static WeatherData getWeather() {
-		String apiKey = "";
-		String city = "";
-		String country = "";
-		String zmw = "";
-		Properties props = new Properties();
-		try {
-			File file = new File("weather.properties");
-			LogManager.getLogger(WeatherService.class).info("Weather properties: {}", file.getAbsolutePath());
-			FileReader fileReader = new FileReader(file);
+    public static WeatherData getWeather() {
+        String apiKey = "";
+        String city = "";
+        String country = "";
+        String zmw = "";
+        Properties props = new Properties();
+        File file = new File("weather.properties");
+        try (FileReader fileReader = new FileReader(file)) {
 
-			props.load(fileReader);
-			apiKey = props.getProperty("apiKey");
-			city = props.getProperty("city");
-			country = props.getProperty("country");
-			zmw = props.getProperty("zmw");
-		} catch (IOException e) {
-			LogManager.getLogger(WeatherService.class).info("Could not find weather properties!");
-			return null;
-		}
+            LogManager.getLogger(WeatherService.class).info("Weather properties: {}", file.getAbsolutePath());
 
-		WeatherData weatherData = new WeatherData();
+            props.load(fileReader);
+            apiKey = props.getProperty("apiKey");
+            city = props.getProperty("city");
+            country = props.getProperty("country");
+            zmw = props.getProperty("zmw");
+        } catch (IOException e) {
+            LogManager.getLogger(WeatherService.class).info("Could not find weather properties!");
+            return null;
+        }
 
-		WeatherRequest req = new WeatherRequest();
-		req.setZmw(zmw);
-		req.setApiKey(apiKey);
-		req.addFeature(Feature.CONDITIONS);
-		req.addFeature(Feature.FORECAST);
-		WeatherResponse weather = req.query(country, city);
-		LogManager.getLogger(WeatherData.class).debug(weather.getResponse());
+        WeatherData weatherData = new WeatherData();
 
-		if (weather.getCurrent_observation() != null) {
-			weatherData.setHumidity(weather.getCurrent_observation().getRelativeHumidity().replace("%", ""));
-			weatherData.setTempC(weather.getCurrent_observation().getTempC());
-			weatherData.setPressure(Float.toString(weather.getCurrent_observation().getPressureMb()));
-			LogManager.getLogger(WeatherService.class).debug(weatherData);
-		} else {
-			LogManager.getLogger(WeatherService.class).info("Could not access weather information");
-		}
+        WeatherRequest req = new WeatherRequest();
+        req.setZmw(zmw);
+        req.setApiKey(apiKey);
+        req.addFeature(Feature.CONDITIONS);
+        req.addFeature(Feature.FORECAST);
+        WeatherResponse weather = req.query(country, city);
+        LogManager.getLogger(WeatherData.class).debug(weather.getResponse());
 
-		return weatherData;
-	}
+        if (weather.getCurrent_observation() != null) {
+            weatherData.setHumidity(weather.getCurrent_observation().getRelativeHumidity().replace("%", ""));
+            weatherData.setTempC(weather.getCurrent_observation().getTempC());
+            weatherData.setPressure(Float.toString(weather.getCurrent_observation().getPressureMb()));
+            LogManager.getLogger(WeatherService.class).debug(weatherData);
+        } else {
+            LogManager.getLogger(WeatherService.class).info("Could not access weather information");
+        }
 
-	public static void main(String[] args) {
-		WeatherData weather = WeatherService.getWeather();
-		if (weather != null) {
-			LogManager.getLogger(WeatherService.class).info(weather.getTempC());
-			LogManager.getLogger(WeatherService.class).info(weather.getHumidity());
-		}
-	}
+        return weatherData;
+    }
+
+    public static void main(String[] args) {
+        WeatherData weather = WeatherService.getWeather();
+        if (weather != null) {
+            LogManager.getLogger(WeatherService.class).info(weather.getTempC());
+            LogManager.getLogger(WeatherService.class).info(weather.getHumidity());
+        }
+    }
 }
