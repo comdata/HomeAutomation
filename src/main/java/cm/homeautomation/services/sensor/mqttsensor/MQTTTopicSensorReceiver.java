9ba2cf749c8c518591d44bba8bd976fc12850690
Christoph Mertins <c.mertins@gmail.com>
2020-10-29 09:17:31 +0100

    enable logging in mqtt sensor

--- src/main/java/cm/homeautomation/services/sensor/mqttsensor/MQTTTopicSensorReceiver.java
+++ src/main/java/cm/homeautomation/services/sensor/mqttsensor/MQTTTopicSensorReceiver.java
@@ -6,8 +6,8 @@ import javax.enterprise.context.ApplicationScoped;
 import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
 
-import org.apache.log4j.LogManager;
 import org.greenrobot.eventbus.Subscribe;
+import org.jboss.logging.Logger;
 
 import cm.homeautomation.db.EntityManagerService;
 import cm.homeautomation.entities.Sensor;
@@ -15,10 +15,11 @@ import cm.homeautomation.eventbus.EventBusService;
 import cm.homeautomation.mqtt.topicrecorder.MQTTTopicEvent;
 import cm.homeautomation.services.sensors.SensorDataLimitViolationException;
 import cm.homeautomation.services.sensors.Sensors;
-import lombok.extern.log4j.Log4j2;
 
 @ApplicationScoped
 public class MQTTTopicSensorReceiver {
+
+	private static final Logger LOG = Logger.getLogger(MQTTTopicSensorReceiver.class);
 
 	public MQTTTopicSensorReceiver() {
 		EventBusService.getEventBus().register(this);
@@ -26,14 +27,14 @@ public class MQTTTopicSensorReceiver {
 
 	@Subscribe
 	public void receiveMQTTTopic(MQTTTopicEvent topicEvent) {
-//		log.debug("got topic: " + topicEvent.getTopic() + " -  message: " + topicEvent.getMessage());
+		LOG.debug("got topic: " + topicEvent.getTopic() + " -  message: " + topicEvent.getMessage());
 
 		EntityManager em = EntityManagerService.getManager();
 
 		String technicalType = "mqtt://" + topicEvent.getTopic();
 		try {
-			
-//			LogManager.getLogger(this.getClass()).debug("looking for technicaltype: " + technicalType);
+
+			LOG.debug("looking for technicaltype: " + technicalType);
 
 			List<Sensor> sensors = em
 					.createQuery("select s from Sensor s where s.sensorTechnicalType=:technicalType", Sensor.class)
@@ -42,23 +43,21 @@ public class MQTTTopicSensorReceiver {
 			if (sensors != null && !sensors.isEmpty()) {
 
 				try {
-					
-					Sensor sensor=sensors.get(0);
-//					LogManager.getLogger(this.getClass())
-//							.debug("got sensors: " + sensor.getId() + " name:" + sensor.getSensorName());
-					
+
+					Sensor sensor = sensors.get(0);
+					LOG.debug("got sensors: " + sensor.getId() + " name:" + sensor.getSensorName());
+
 					Sensors.getInstance().saveSensorData(sensor.getId(), topicEvent.getMessage());
 				} catch (SensorDataLimitViolationException e) {
-//					LogManager.getLogger(this.getClass()).error(e);
-//					log.error(e);
+					LOG.error(e);
 				}
 			} else {
-//				LogManager.getLogger(this.getClass()).debug("list is empty for technicaltype: " + technicalType);
+				LOG.debug("list is empty for technicaltype: " + technicalType);
 
 			}
 
 		} catch (NoResultException e) { // ignore since no result is a valid expectation here
-//			log.debug(e);
+			LOG.debug(e);
 		}
 
 	}
