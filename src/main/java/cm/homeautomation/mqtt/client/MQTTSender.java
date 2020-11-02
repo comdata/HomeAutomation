70fe3514df30db38db105dfcb39e0844bde548ca
Christoph Mertins <c.mertins@gmail.com>
2020-11-02 09:31:14 +0100

    remove sysout

--- src/main/java/cm/homeautomation/mqtt/client/MQTTSender.java
+++ src/main/java/cm/homeautomation/mqtt/client/MQTTSender.java
@@ -63,7 +63,7 @@ public class MQTTSender {
 	}
 
 	public void doSendSyncMQTTMessage(String topic, String messagePayload) {
-		System.out.println("MQTT OUTBOUND" + topic + " " + messagePayload);
+		//System.out.println("MQTT OUTBOUND " + topic + " " + messagePayload);
 	
 		bus.publish("MQTTSendEvent", new MQTTSendEvent(topic, messagePayload));
 
