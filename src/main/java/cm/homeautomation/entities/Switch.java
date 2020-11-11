488eecbd4396c150eed545f4fc4d0ba5669f799e
Christoph Mertins <c.mertins@gmail.com>
2020-11-11 15:46:44 +0100

    lombok

--- src/main/java/cm/homeautomation/entities/Switch.java
+++ src/main/java/cm/homeautomation/entities/Switch.java
@@ -22,16 +22,20 @@ import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 
+import lombok.Getter;
+import lombok.Setter;
 
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "@class")
-@JsonSubTypes({ @JsonSubTypes.Type(value = MQTTSwitch.class, name = "MQTTSwitch"),
-	})
+@JsonSubTypes({ @JsonSubTypes.Type(value = MQTTSwitch.class, name = "MQTTSwitch"), })
 @Entity
+@Getter
+@Setter
+
 public class Switch {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	// Dates
 	private Date dateInstalled;
 
@@ -58,9 +62,9 @@ public class Switch {
 	@JsonManagedReference("referencedSwitch")
 	private List<Light> lights;
 
-	@JoinColumn(nullable = true, referencedColumnName="ID")
-	@OneToOne(optional=true)
-	
+	@JoinColumn(nullable = true, referencedColumnName = "ID")
+	@OneToOne(optional = true)
+
 	private IRCommand irCommand;
 
 	@Column(name = "SWITCH_SET_URL")
@@ -71,14 +75,14 @@ public class Switch {
 
 	@OneToOne
 	@JoinColumn(name = "SENSOR_ID")
-	
+
 	private Sensor sensor;
 
 	@ManyToOne
 	@JoinColumn(name = "ROOM_ID")
 	@XmlTransient
 	@JsonIgnore
-	
+
 	private Room room;
 
 	@Column(name = "VISIBLE", nullable = false)
@@ -87,41 +91,9 @@ public class Switch {
 	@Transient
 	private boolean switchState;
 
-	public String getHouseCode() {
-		return houseCode;
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public IRCommand getIrCommand() {
-		return irCommand;
-	}
-
-	public String getLatestStatus() {
-		return latestStatus;
-	}
-
-	public Date getLatestStatusFrom() {
-		return latestStatusFrom;
-	}
-
 	@XmlIDREF
 	public List<Light> getLights() {
 		return lights;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public String getOffCode() {
-		return offCode;
-	}
-
-	public String getOnCode() {
-		return onCode;
 	}
 
 	@XmlTransient
@@ -135,168 +107,9 @@ public class Switch {
 		return sensor;
 	}
 
-	public String getSubType() {
-		return subType;
-	}
-
-	public String getSwitchNo() {
-		return switchNo;
-	}
-
-	public String getSwitchOffCode() {
-		return switchOffCode;
-	}
-
-	public String getSwitchOnCode() {
-		return switchOnCode;
-	}
-
-	public String getSwitchSetUrl() {
-		return switchSetUrl;
-	}
-
-	public String getSwitchType() {
-		return switchType;
-	}
-
-	public String getTargetStatus() {
-		return targetStatus;
-	}
-
-	public boolean isSwitchState() {
-		return switchState;
-	}
-
-	public boolean isVisible() {
-		return visible;
-	}
-
-	public void setHouseCode(final String houseCode) {
-		this.houseCode = houseCode;
-	}
-
-	public void setId(final Long id) {
-		this.id = id;
-	}
-
-	public void setIrCommand(final IRCommand irCommand) {
-		this.irCommand = irCommand;
-	}
-
-	public void setLatestStatus(final String latestStatus) {
-		this.latestStatus = latestStatus;
-	}
-
-	public void setLatestStatusFrom(final Date latestStatusFrom) {
-		this.latestStatusFrom = latestStatusFrom;
-	}
-
 	@XmlIDREF
 	public void setLights(final List<Light> lights) {
 		this.lights = lights;
 	}
 
-	public void setName(final String name) {
-		this.name = name;
-	}
-
-	public void setOffCode(final String offCode) {
-		this.offCode = offCode;
-	}
-
-	public void setOnCode(final String onCode) {
-		this.onCode = onCode;
-	}
-
-	public void setRoom(final Room room) {
-		this.room = room;
-	}
-
-	public void setSensor(final Sensor sensor) {
-		this.sensor = sensor;
-	}
-
-	public void setSubType(final String subType) {
-		this.subType = subType;
-	}
-
-	public void setSwitchNo(final String switchNo) {
-		this.switchNo = switchNo;
-	}
-
-	public void setSwitchOffCode(final String switchOffCode) {
-		this.switchOffCode = switchOffCode;
-	}
-
-	public void setSwitchOnCode(final String switchOnCode) {
-		this.switchOnCode = switchOnCode;
-	}
-
-	public void setSwitchSetUrl(final String switchSetUrl) {
-		this.switchSetUrl = switchSetUrl;
-	}
-
-	public void setSwitchState(final boolean switchState) {
-		this.switchState = switchState;
-	}
-
-	public void setSwitchType(final String switchType) {
-		this.switchType = switchType;
-	}
-
-	public void setTargetStatus(final String targetStatus) {
-		this.targetStatus = targetStatus;
-	}
-	
-	public void setVisible(boolean visible) {
-		this.visible = visible;
-	}
-
-	public Date getDateInstalled() {
-		return dateInstalled;
-	}
-
-	public void setDateInstalled(Date dateInstalled) {
-		this.dateInstalled = dateInstalled;
-	}
-
-	public Date getDateLastSeen() {
-		return dateLastSeen;
-	}
-
-	public void setDateLastSeen(Date dateLastSeen) {
-		this.dateLastSeen = dateLastSeen;
-	}
-
-	public String getManufacturer() {
-		return manufacturer;
-	}
-
-	public void setManufacturer(String manufacturer) {
-		this.manufacturer = manufacturer;
-	}
-
-	public String getType() {
-		return type;
-	}
-
-	public void setType(String type) {
-		this.type = type;
-	}
-
-	public String getFirmware() {
-		return firmware;
-	}
-
-	public void setFirmware(String firmware) {
-		this.firmware = firmware;
-	}
-
-	public String getExternalId() {
-		return externalId;
-	}
-
-	public void setExternalId(String externalId) {
-		this.externalId = externalId;
-	}
 }
