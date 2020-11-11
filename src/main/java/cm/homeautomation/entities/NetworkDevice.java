9aa3419d043d5e3d7ab24447602412ad3903fd8e
Christoph Mertins <c.mertins@gmail.com>
2020-11-11 15:49:48 +0100

    add lombok

--- src/main/java/cm/homeautomation/entities/NetworkDevice.java
+++ src/main/java/cm/homeautomation/entities/NetworkDevice.java
@@ -7,7 +7,12 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import lombok.Getter;
+import lombok.Setter;
+
 @Entity
+@Getter
+@Setter
 public class NetworkDevice {
 
 	@Id
@@ -18,43 +23,4 @@ public class NetworkDevice {
 	private String hostname;
 	private Date lastSeen;
 
-	public String getMac() {
-		return mac;
-	}
-
-	public void setMac(String mac) {
-		this.mac = mac;
-	}
-
-	public String getIp() {
-		return ip;
-	}
-
-	public void setIp(String ip) {
-		this.ip = ip;
-	}
-
-	public String getHostname() {
-		return hostname;
-	}
-
-	public void setHostname(String hostname) {
-		this.hostname = hostname;
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public Date getLastSeen() {
-		return lastSeen;
-	}
-
-	public void setLastSeen(Date lastSeen) {
-		this.lastSeen = lastSeen;
-	}
 }
