c587e75653869d763ad1e205c887d6de0da8f6d0
Christoph Mertins <c.mertins@gmail.com>
2020-12-03 17:33:13 +0100

    imports

--- src/main/java/cm/homeautomation/entities/IRCommand.java
+++ src/main/java/cm/homeautomation/entities/IRCommand.java
@@ -6,7 +6,6 @@ import javax.persistence.CollectionTable;
 import javax.persistence.Column;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
