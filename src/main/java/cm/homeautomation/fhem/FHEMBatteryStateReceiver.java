767659fe88491bbebac19757d464e8369e8b2b85
Christoph Mertins <c.mertins@gmail.com>
2020-12-11 08:49:52 +0100

    cleanup

--- src/main/java/cm/homeautomation/fhem/FHEMBatteryStateReceiver.java
+++ src/main/java/cm/homeautomation/fhem/FHEMBatteryStateReceiver.java
@@ -6,9 +6,7 @@ import java.util.List;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
-import javax.inject.Singleton;
 import javax.persistence.EntityManager;
-import javax.transaction.Transactional;
 
 import cm.homeautomation.entities.FHEMDevice;
 import cm.homeautomation.entities.ManualTask;
