e5c77a1144f7ed62cf220860531b2cb920265d23
Christoph Mertins <c.mertins@gmail.com>
2020-12-28 23:27:47 +0100

    cleanup

--- src/main/java/cm/homeautomation/services/scheduler/SingleJobClass.java
+++ src/main/java/cm/homeautomation/services/scheduler/SingleJobClass.java
@@ -20,29 +20,17 @@ public class SingleJobClass implements Job {
 	public SingleJobClass() {
 		super();
 	}
-	
-	
 
 	@Override
 	public void execute(JobExecutionContext context) throws JobExecutionException {
 
 		String clazz = (String) context.getJobDetail().getJobDataMap().get("clazz");
-		System.out.println("clazz: "+clazz);
 		String[] clazzParts = clazz.split("\\.");
-		System.out.println(clazzParts.length);
-		String shortClazz = clazzParts[clazzParts.length-1];
+		String shortClazz = clazzParts[clazzParts.length - 1];
+		@SuppressWarnings("unchecked")
 		List<String> argumentList = (List<String>) context.getJobDetail().getJobDataMap().get("arguments");
 
 		JobArguments jobArguments = new JobArguments(argumentList);
-		
-		System.out.println(argumentList);
-
-		for (String object2 : argumentList) {
-			System.out.println(object2);
-		}
-
-		String[] arguments = argumentList.toArray(new String[0]);
-		System.out.println(arguments);
 
 		bus.publish(shortClazz, jobArguments);
 
