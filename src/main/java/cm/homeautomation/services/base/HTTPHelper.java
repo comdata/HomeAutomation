c2d3d55811cafd00aad6d7528ca6b8974e4b2023
Christoph Mertins <c.mertins@gmail.com>
2020-12-29 13:32:14 +0100

    switch http implementation

--- src/main/java/cm/homeautomation/services/base/HTTPHelper.java
+++ src/main/java/cm/homeautomation/services/base/HTTPHelper.java
@@ -3,15 +3,14 @@ package cm.homeautomation.services.base;
 import java.io.IOException;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
 
-import org.apache.http.HttpResponse;
-import org.apache.http.auth.AuthScope;
-import org.apache.http.auth.UsernamePasswordCredentials;
-import org.apache.http.client.CredentialsProvider;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.BasicCredentialsProvider;
-import org.apache.http.impl.client.HttpClientBuilder;
+import org.eclipse.microprofile.context.ManagedExecutor;
+
+import com.google.api.client.http.GenericUrl;
+import com.google.api.client.http.HttpRequest;
+import com.google.api.client.http.HttpRequestFactory;
+import com.google.api.client.http.javanet.NetHttpTransport;
 
 /**
  * http helper class with credentials
@@ -22,36 +21,25 @@ import org.apache.http.impl.client.HttpClientBuilder;
 @ApplicationScoped
 public class HTTPHelper {
 
-//	public static void performHTTPRequest(String url) {
-//		performHTTPRequest(url, null);
-//	}
+	@Inject
+	ManagedExecutor executor;
 
 	public void performHTTPRequest(String url) {
 
 		final Runnable httpRequestThread = () -> {
 			try {
-//				//LogManager.getLogger(HTTPHelper.class).debug("perform http call: " + url);
 
-				final HttpGet getMethod = new HttpGet(url);
-				final HttpClientBuilder clientBuilder = HttpClientBuilder.create();
-
-				final String[] userPassword = url.split("@")[0].replace("http://", "").split(":");
-
-				final CredentialsProvider credsProvider = new BasicCredentialsProvider();
-				credsProvider.setCredentials(new AuthScope(getMethod.getURI().getHost(), getMethod.getURI().getPort()),
-						new UsernamePasswordCredentials(userPassword[0], userPassword[1]));
-				clientBuilder.setDefaultCredentialsProvider(credsProvider);
-				final HttpClient httpClient = clientBuilder.build();
-
-				final HttpResponse httpResponse = httpClient.execute(getMethod);
-
+				HttpRequestFactory requestFactory = new NetHttpTransport().createRequestFactory();
+				HttpRequest request = requestFactory.buildGetRequest(new GenericUrl(url));
+				String rawResponse = request.execute().parseAsString();
+				System.out.println(rawResponse);
 //				//LogManager.getLogger(HTTPHelper.class).debug("http called done: " + httpResponse.getStatusLine());
 
 			} catch (final IOException e) {
 //				//LogManager.getLogger(HTTPHelper.class).error("calling URL: " + url + " failed", e);
 			}
 		};
-		new Thread(httpRequestThread).start();
+		executor.runAsync(httpRequestThread);
 
 	}
 }
