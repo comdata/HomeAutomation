1e8b48df11b97ca435f655d8b3049f3109c8e25c
Christoph Mertins <c.mertins@gmail.com>
2020-12-29 16:22:25 +0100

    add authorization

--- src/main/java/cm/homeautomation/services/base/HTTPHelper.java
+++ src/main/java/cm/homeautomation/services/base/HTTPHelper.java
@@ -8,6 +8,7 @@ import javax.inject.Inject;
 import org.eclipse.microprofile.context.ManagedExecutor;
 
 import com.google.api.client.http.GenericUrl;
+import com.google.api.client.http.HttpHeaders;
 import com.google.api.client.http.HttpRequest;
 import com.google.api.client.http.HttpRequestFactory;
 import com.google.api.client.http.javanet.NetHttpTransport;
@@ -31,8 +32,15 @@ public class HTTPHelper {
 
 				HttpRequestFactory requestFactory = new NetHttpTransport().createRequestFactory();
 				HttpRequest request = requestFactory.buildGetRequest(new GenericUrl(url));
+
+				if (url.contains("@") && url.contains(":")) {
+					final String[] userPassword = url.split("@")[0].replace("http://", "").split(":");
+					HttpHeaders headers = new HttpHeaders();
+					headers.setBasicAuthentication(userPassword[0], userPassword[1]);
+					request.setHeaders(headers);
+				}
 				String rawResponse = request.execute().parseAsString();
-				System.out.println(rawResponse);
+				System.out.println("HTTP Response: " + rawResponse);
 //				//LogManager.getLogger(HTTPHelper.class).debug("http called done: " + httpResponse.getStatusLine());
 
 			} catch (final IOException e) {
